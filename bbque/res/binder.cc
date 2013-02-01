3ebf8e139b0630689cd4e527f9ab334ee515a1f5
Giuseppe Massari <joe.massanga@gmail.com>
2013-02-01 16:44:02 +0100

    [RB] Introduce class ResourceBinder
    This is a static class providing methods to solve or provide information
    about resource bindings.
    
    The binding is a mandatory step, through which a scheduling policy links
    the resources requested in the AWMs (referenced by a resource path built
    from the recipe) to the system resources (referenced by the resource path
    registered by the Platform Proxy.
    
    Previously, class WorkingMode embedded code for performing this step.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ bbque/res/binder.cc
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <utility>
+
+#include "bbque/resource_accounter.h"
+#include "bbque/res/binder.h"
+#include "bbque/res/resource_path.h"
+
+
+#define MODULE_NAMESPACE 	"bq.rb"
+
+namespace bbque { namespace res {
+
+
+uint32_t ResourceBinder::Bind(
+		UsagesMap_t const & src_um,
+		ResourceIdentifier::Type_t r_type,
+		ResID_t	src_r_id,
+		ResID_t dst_r_id,
+		UsagesMapPtr_t dst_pum) {
+	ResourceAccounter &ra(ResourceAccounter::GetInstance());
+	UsagesMap_t::const_iterator src_it, src_end;
+	ResourcePath::ExitCode_t rp_result;
+	uint32_t count = 0;
+
+	// Sanity check
+	if (r_type >= ResourceIdentifier::TYPE_COUNT)
+		return 0;
+
+	// Proceed with the resource binding...
+	src_end = src_um.end();
+	for (src_it = src_um.begin(); src_it != src_um.end(); ++src_it) {
+		ResourcePathPtr_t const & src_ppath(src_it->first);
+		UsagePtr_t const        & src_pusage(src_it->second);
+
+		// Replace ID of the given resource type with a new ID
+		ResourcePathPtr_t dst_ppath(new ResourcePath(src_ppath->ToString()));
+		if (dst_ppath->NumLevels() == 0)
+			return 0;
+		rp_result = dst_ppath->ReplaceID(r_type, src_r_id, dst_r_id);
+
+		// Set the bit corresponding to the bound ID
+		if (rp_result == ResourcePath::OK) {
+			DB(fprintf(stderr, FD("Bind: [%s] to [%s] done\n"),
+					src_ppath->ToString().c_str(), dst_ppath->ToString().c_str());
+			);
+			++count;
+		}
+		else {
+			DB(fprintf(stderr, FD("Bind: Nothing to do in [%s]\n"),
+					src_ppath->ToString().c_str());
+			);
+		}
+
+		// Create a new Usage object and set the binding list
+		UsagePtr_t dst_pusage(new Usage(src_pusage->GetAmount()));
+		dst_pusage->SetBindingList(ra.GetResources(dst_ppath));
+
+		// Insert the resource usage object in the output map
+		dst_pum->insert(
+				std::pair<ResourcePathPtr_t, UsagePtr_t>(dst_ppath, dst_pusage));
+	}
+	return count;
+}
+
+BindingBitset ResourceBinder::GetMask(
+		UsagesMapPtr_t pum,
+		ResourceIdentifier::Type_t r_type) {
+	UsagesMap_t::iterator pum_it;
+	BindingBitset binding_mask;
+	ResID_t r_id;
+
+	// Sanity check
+	if (r_type >= ResourceIdentifier::TYPE_COUNT)
+		return binding_mask;
+
+	// Scan the resource usages map
+	for (pum_it = pum->begin();	pum_it != pum->end(); ++pum_it) {
+		ResourcePathPtr_t const & ppath(pum_it->first);
+		// Get the ID of the resource type in the path
+		r_id = ppath->GetID(r_type);
+		if ((r_id == R_ID_NONE) || (r_id == R_ID_ANY))
+			continue;
+		// Set the ID-th bit in the mask
+		binding_mask.set(r_id);
+	}
+	return binding_mask;
+}
+
+ResourceBinder::ExitCode_t ResourceBinder::Compatible(
+		UsagesMapPtr_t src_pum,
+		UsagesMapPtr_t dst_pum) {
+	UsagesMap_t::iterator src_it, src_end;
+	UsagesMap_t::iterator dst_it, dst_end;
+
+	// Different size of the maps -> not compatible
+	if (src_pum->size() != dst_pum->size())
+		return NOT_COMPATIBLE;
+
+	// Compare each resource path
+	for (src_it = src_pum->begin(), dst_it = dst_pum->begin();
+			src_it != src_end; ++src_it, ++dst_it) {
+		ResourcePathPtr_t const & src_ppath(src_it->first);
+		ResourcePathPtr_t const & dst_ppath(dst_it->first);
+		// The path should be equal or have equal types (template)
+		if (src_ppath->Compare(*(dst_ppath.get())) == ResourcePath::NOT_EQUAL) {
+			return NOT_COMPATIBLE;
+		}
+	}
+
+	return OK;
+}
+
+} // namespace res
+
+} // namespace bbque
+
---
+++ include/bbque/res/binder.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BBQUE_RESOURCE_BINDER_H_
+#define BBQUE_RESOURCE_BINDER_H_
+
+#include <cstdint>
+
+#include "bbque/res/usage.h"
+
+
+namespace bbque { namespace res {
+
+/**
+ * @class ResourceBinder
+ *
+ * This a static class providing methods to solve or provide information about
+ * resource bindings.
+ *
+ * The binding is a mandatory step, through which a scheduling policy links
+ * the resources requested in the AWMs (referenced by a resource path built
+ * from the recipe) to the system resources (referenced by the resource path
+ * registered by the Platform Proxy.
+ */
+class ResourceBinder {
+
+public:
+
+	/**
+	 * @enum Exit codes
+	 */
+	enum ExitCode_t {
+		OK         = 0 ,
+		NOT_COMPATIBLE
+	};
+
+	/**
+	 * @brief Bind resource usages to system resources
+	 *
+	 * @param src_um The map of resource usages to bind
+	 * @param r_type The type of resource to bind
+	 * @param src_r_id The ID of the resource to bind
+	 * @param dst_r_id The ID of the system resource to which bind
+	 * @param dst_pum A shared pointer to the map of bound resources to fill
+	 *
+	 * @return The number of resources on which the binding has been performed
+	 */
+	static uint32_t Bind(
+			UsagesMap_t const & src_um,
+			ResourceIdentifier::Type_t r_type,
+			ResID_t	src_r_id,
+			ResID_t dst_r_id,
+			UsagesMapPtr_t dst_pum);
+
+	/**
+	 * @brief Retrieve IDs of a type of resource from a UsagesMap_t
+	 *
+	 * @param pum A shared pointer to the map of resource usages
+	 * @param r_type The type of resource to consider
+	 *
+	 * @return A bitset object where in the set bit are related to the ID to
+	 * which the resource usage has been bound
+	 */
+	static BindingBitset GetMask(
+			UsagesMapPtr_t pum, ResourceIdentifier::Type_t r_type);
+
+	/**
+	 * @brief Check if two resource usages map are compatible for binding
+	 *
+	 * @param src_pum A shared pointer to the first map of resource usages
+	 * @param dst_pum A shared pointer to the second map of resource usages
+	 *
+	 * @return OK if compatible, NOT_COMPATIBLE otherwise
+	 */
+	static ExitCode_t Compatible(
+			UsagesMapPtr_t src_pum, UsagesMapPtr_t dst_pum);
+};
+
+
+} // namespace res
+
+} // namespace bbque
+
+#endif // BBQUE_RESOURCE_BINDER_H_
+
+
