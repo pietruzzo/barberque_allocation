856a24802e7ae6af3a3b0fd63d84f3a522ac4d8a
Patrick Bellasi <derkling@gmail.com>
2013-04-04 13:02:29 +0200

    [Tools_SignalAnalysis] Switch to status and metrics variables
    This is a main refactoring targeting the usage of share status and metrics
    variable which could now be defined and customized for each different
    interesting metrics apart from the GoalGap.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- tools/signalAnalysisPlotter.sh
+++ tools/signalAnalysisPlotter.sh
@@ -123,22 +123,58 @@ testNormal $1 $2 $3 | \
 ################################################################################
 # Exp Moving Average
 ################################################################################
-EMA=0
-ALPHA=0
 emaInit() {
-ALPHA=$(calc "2.0 / ($1 + 1)")
+SAMPLES=$1
+calc "2.0 / ($SAMPLES + 1)"
 }
 
 emaUpdate() {
-EMA=$(calc "($ALPHA * $1) + ((1 - $ALPHA) * $EMA)")
+ALPHA=$1
+EMA=$2
+NEW=$3
+calc "($ALPHA * $NEW) + ((1 - $ALPHA) * $EMA)"
 }
 
-emaReset() {
-EMA=0
-}
+################################################################################
+# Simulation Model Status and Metrics
+################################################################################
+# Model Status
+S_CPS=20
+S_CPS_SLEEP=0      # Sleep time for configured CPS
+S_RND_GG_MIN=20    # GG Random Generator Mimimum
+S_RND_GG_MAX=60    # GG Random Generator Maximum
+S_RND_GG_DLT=40    # GG Random Generator Delta, i.e. Min-Max
+S_GG_EMA_SMPLS=60  # GG EMA Samples number
+S_GG_EMA_ALPHA=0   # GG EMA Alpha value
+S_PTME=0     # Previous Cycle Time
+S_CTME=0     # Current Cycle Time
+S_CAWM=0     # Current AWM
+# Model Metrics
+M_GGVAR=0    # GoalGap Variation
+M_GGEMA=0    # GoalGap EMA
+M_GGTRD=5    # GoalGap Threshold
+M_GGPRV=0    # GoalGap Previous
+M_GGCUR=3    # GoalGap Previous
+M_RREMA=0    # Reconfiguration Rate EMA
+M_RRTRD=5    # Reconfiguration Rate Threshold
 
-emaValue() {
-$EMA
+# Model initialization
+S_CPS_SLEEP=$(calc "1 / $S_CPS")
+S_GG_EMA_ALPHA=$(emaInit  $S_GG_EMA_SMPLS)
+
+################################################################################
+# Working Mode Management
+################################################################################
+
+CURR_AWM=1
+CURR_GG=0
+
+setLowAWM() {
+CURR_AWM=0
+}
+setHighAWM() {
+CURR_AWM=1
+CURR_GG=40
 }
 
 
@@ -146,85 +182,96 @@ $EMA
 # Random Generator
 ################################################################################
 
+# Report current configuration
+cfgReport() {
+echo "*** Current configuration"
+echo "GoalGap($S_RND_GG_MIN,$S_RND_GG_MAX), EMA Samples: $S_GG_EMA_SMPLS"
+echo "CPS: $S_CPS"
+}
+
 # Configure the random numbers generator
-# $1 - MIN value
-# $2 - MAX value
+# $1 - GG MIN value
+# $2 - GG MAX value
+# $3 - GG EMA Samples
 # $3 - CPS value
-# $4 - RRT value
-rndSetup() {
-MIN=$1
-MAX=$(rpn "$2 $1 -")
-SLEEP=$(calc "1 / $3")
-GGTRLD=$4
+cfgUpdate() {
+S_RND_GG_MIN=$1
+S_RND_GG_MAX=$2
+S_RND_GG_DLT=$(calc "$2 - $1")
+S_GG_EMA_SMPLS=$3
+S_GG_EMA_ALPHA=$(emaInit  $3)
+S_CPS=$4
+S_CPS_SLEEP=$(calc "1 / $4")
+cfgReport
 }
 
-rndUpdate() {
-rndSetup $1 $2 $3 $5
-emaInit $4
-}
-
-RNDCFG=$(mktemp -u /tmp/bbqueSignalAlalyzer_rndcfg_XXXXXX)
-mkfifo $RNDCFG
+CFGFIFO=$(mktemp -u /tmp/bbqueSignalAlalyzer_rndcfg_XXXXXX)
+mkfifo $CFGFIFO
 
 ################################################################################
 # Custom Samplers for GoalGap Variance
 ################################################################################
 
-PGG=0
-
 # Compute GG variation
-# $1 - the previous GG value
-# $2 - the current GG value
-# return: (Variation, PreviousGG)
+# return: (CurrentGG, VariationGG)
 ggVarGenerator() {
-NGG=$(calc "$MIN + (($MAX * $2) / 65536)")
-VAR=$(calc "v=$1-$2; if (v<0) -v else v")
-echo "$VAR $2"
+RND=$(random)
+M_GGCUR=$(calc "$S_RND_GG_MIN + (($S_RND_GG_DLT * $RND) / 65536)")
+M_GGVAR=$(calc "v=$M_GGCUR-$M_GGPRV; if (v<0) -v else v")
+M_GGPRV=$M_GGCUR
 }
+
+################################################################################
+# AWM Assignement Model
+################################################################################
+
+# Update the current AWM model, where:
+updateAWM() {
+
+    #echo "Old AWM Status: $S_PTME, $S_CTME, $S_CAWM; Metrics: $M_GGEMA, $M_GGTRD, $M_RREMA, $M_RRTRD"
+
+    # Compute new GG Variation
+    ggVarGenerator
+
+    #echo "New AWM Status: $S_PTME, $S_CTME, $S_CAWM; Metrics: $M_GGEMA, $M_GGTRD, $M_RREMA, $M_RRTRD"
+
+}
+
 
 ################################################################################
 # A Samples Generator
 ################################################################################
 
 
-PLOT_GG=$(mktemp -u /tmp/bbqueSignalAlalyzer_plot_XXXXXX)
+PLOT_AWM=$(mktemp -u /tmp/bbqueSignalAlalyzer_plotAWM_XXXXXX)
+mkfifo $PLOT_AWM
+PLOT_GG=$(mktemp -u /tmp/bbqueSignalAlalyzer_plotGG_XXXXXX)
 mkfifo $PLOT_GG
-PLOT_VF=$(mktemp -u /tmp/bbqueSignalAlalyzer_plot_XXXXXX)
+PLOT_VF=$(mktemp -u /tmp/bbqueSignalAlalyzer_plotGGFilter_XXXXXX)
 mkfifo $PLOT_VF
 
-# A configurable source generator:
-# $1 - sampler used to generate numbers
-# $2 - samples name
+# The data generator
 dataSource() {
 while true; do
 
-    # Compute a new random value
-    RN=$(random)
-    V1=$(rpn "$MAX $RN * 65536 / $MIN +")
-
-    RESULT=($($1 $PGG $V1))
-    GGVAR=${RESULT[0]}
-    PGG=${RESULT[1]}
-
     # Get current time
-    TIME=$(getTime)
+    S_CTME=$(getTime)
+
+    # Update AWM Model
+    updateAWM
 
     # Update EMA
-    emaUpdate $GGVAR
+    M_GGEMA=$(emaUpdate $S_GG_EMA_ALPHA $M_GGEMA $M_GGVAR)
 
     # Dump metrics
-    printf "%9.6f GoalGap %7.3f\n" $TIME $V1 >$PLOT_GG
-    printf "%9.6f GGTrld %7.3f $2 %7.3f EMA %7.3f\n" $TIME $GGTRLD $GGVAR $EMA >$PLOT_VF
+    printf "%9.6f CurrAWM %2d\n" $S_CTME $S_CAWM >$PLOT_AWM
+    printf "%9.6f GoalGap %7.3f\n" $S_CTME $M_GGCUR >$PLOT_GG
+    printf "%9.6f GG_Thr %7.3f GG_Var %7.3f GG_EMA %7.3f\n" $S_CTME $M_GGTRD $M_GGVAR $M_GGEMA >$PLOT_VF
 
-    # Wait for next sample
-    #sleep $SLEEP;
-    read -t $SLEEP <>$RNDCFG SETTINGS && rndUpdate $SETTINGS
+    # Wait for next sample, or update the configuration
+    read -t $S_CPS_SLEEP <>$CFGFIFO SETTINGS && cfgUpdate $SETTINGS
 
 done
-}
-
-ggVarSource() {
-dataSource ggVarGenerator "GoalGapVariation"
 }
 
 # A configurable plotting function, where:
@@ -234,6 +281,7 @@ dataSource ggVarGenerator "GoalGapVariation"
 # $4 - Y max (or - for autoscale)
 # $5 - Geometry
 plotData() {
+YMAX=""
 [ $4 != "-" ] && YMAX="--ymax=$4"
 tail -n0 -f "$1" | \
 	feedgnuplot \
@@ -251,15 +299,14 @@ tail -n0 -f "$1" | \
 ################################################################################
 # Command line processing
 ################################################################################
-tuneGenerator() {
-[ $1 != '-' ] && [ $1 -lt $MAX_VALUE -o $1 -lt $2 ] && MIN_VALUE=$1
-[ $2 != '-' ] && [ $2 -gt $MIN_VALUE -o $2 -gt $1 ] && MAX_VALUE=$2
-[ $3 != '-' ] && CPS=$3
-[ $4 != '-' ] && EMA_SCALE=$4
-[ $5 != '-' ] && GGTRLD=$5
-echo "$MIN_VALUE $MAX_VALUE $CPS $EMA_SCALE $GGTRLD" >$RNDCFG
-}
 
+tuneGenerator() {
+[ $1 != '-' ] && [ $1 -lt $S_RND_GG_MAX -o $1 -lt $2 ] && S_RND_GG_MIN=$1
+[ $2 != '-' ] && [ $2 -gt $S_RND_GG_MIN -o $2 -gt $1 ] && S_RND_GG_MAX=$2
+[ $3 != '-' ] && S_GG_EMA_SMPLS=$3
+[ $4 != '-' ] && S_CPS=$4
+echo "$S_RND_GG_MIN $S_RND_GG_MAX $S_GG_EMA_SMPLS $S_CPS" >$CFGFIFO
+}
 
 
 ################################################################################
@@ -269,55 +316,47 @@ echo "$MIN_VALUE $MAX_VALUE $CPS $EMA_SCALE $GGTRLD" >$RNDCFG
 usage() {
 echo -e "\t\t***** Signal Test Analyzer *****"
 echo
-echo "Tuning command: <min> <max> <cps> <ema> <gtr>"
-echo "  min - set the minimum value for the random generator"
-echo "  max - set the maximum value for the random generator"
-echo "  cps - set the number of samples per seconds (CPS)"
-echo "  ema - set the number of samples for the Exponential Moving Average (EMA)"
-echo "  gtr - set the GoalGap threshold"
+echo "Tuning command: <gg_min> <gg_max> <gg_ema> <cps>"
+echo "  gg_min - set the minimum value for the GG random generator"
+echo "  gg_max - set the maximum value for the GG random generator"
+echo "  gg_ema - set the number of samples for the GG Exponential Moving Average (EMA)"
+echo "  cps    - set the number of samples per seconds (CPS)"
 echo "Use '-' to keep the previous value"
 echo
 }
 
 plot() {
-# Tuning command memento
+# Tuning command memento and current configuration dump
 usage
-
-# Start data generator
-ggVarSource &
-
+cfgReport
 # Start data plotters
+plotData $PLOT_AWM 'Current AWM (GG) value' 'AWM Value' 4 949x259+-5+193
 plotData $PLOT_GG 'Goal Gap (GG) Value' 'Goal Gap' - 949x246+-5+490
 plotData $PLOT_VF 'GG Variation and EMA' 'GG Variation' 20 949x233+-5-25
-
+# Start data generator
+dataSource &
 }
 
 
 ################################################################################
-# Return if script is just sourced
+# Return on script sourcing or Ctrl+C
 ################################################################################
 
+# Return if the script has been sources
 [[ $_ != $0 ]] && return 2>/dev/null
+
+# Return on Ctrl-+ by cleaning-up all background threads
+cleanup() {
+	kill $(jobs -p)
+	killall feedgnuplot
+	rm $CFGFIFO $PLOT_GG $PLOT_VF
+}
+trap cleanup EXIT
+
 
 ################################################################################
 # Initialization
 ################################################################################
-
-MIN_Y=0
-MAX_Y=80
-
-emaInit  $EMA_SCALE
-rndSetup $MIN_VALUE $MAX_VALUE $CPS 5
-
-
-
-# Clean-up all background threads on exit
-cleanup() {
-	kill $(jobs -p)
-	killall feedgnuplot
-	rm $RNDCFG $PLOT_GG $PLOT_VF
-}
-trap cleanup EXIT
 
 # Main function
 plot
