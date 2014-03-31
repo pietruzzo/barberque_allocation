1fdf56af53f7e9b05f88856ab0c1c190ea8f2f91
Patrick Bellasi <derkling@gmail.com>
2014-03-31 16:37:53 +0200

    [PM] Clean-up compilation warnings
    This is a dummy patch which adds some members usage to avoid
    compilation warning on unused parameters.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- bbque/pm/power_manager.cc
+++ bbque/pm/power_manager.cc
@@ -50,18 +50,24 @@ PowerManager::~PowerManager() {
 
 PowerManager::PMResult PowerManager::GetLoad(
 		ResourcePathPtr_t const & rp, uint32_t &perc) {
+	(void)rp;
+	(void)perc;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 
 PowerManager::PMResult PowerManager::GetTemperature(
 		ResourcePathPtr_t const & rp, uint32_t &celsius) {
+	(void)rp;
+	(void)celsius;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 
 PowerManager::PMResult
 PowerManager::GetClockFrequency(ResourcePathPtr_t const & rp, uint32_t &khz) {
+	(void)rp;
+	(void)khz;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -71,17 +77,25 @@ PowerManager::GetClockFrequencyInfo(
 		uint32_t &khz_min,
 		uint32_t &khz_max,
 		uint32_t &khz_step) {
+	(void)rp;
+	(void)khz_min;
+	(void)khz_max;
+	(void)khz_step;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 PowerManager::PMResult
 PowerManager::SetClockFrequency(ResourcePathPtr_t const & rp, uint32_t khz) {
+	(void)rp;
+	(void)khz;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 
 PowerManager::PMResult
 PowerManager::GetVoltage(ResourcePathPtr_t const & rp, uint32_t &volt) {
+	(void)rp;
+	(void)volt;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -91,6 +105,10 @@ PowerManager::GetVoltageInfo(
 		uint32_t &volt_min,
 		uint32_t &volt_max,
 		uint32_t &volt_step) {
+	(void)rp;
+	(void)volt_min;
+	(void)volt_max;
+	(void)volt_step;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -100,6 +118,9 @@ PowerManager::GetFanSpeed(
 		ResourcePathPtr_t const & rp,
 		FanSpeedType fs_type,
 		uint32_t &value) {
+	(void)rp;
+	(void)fs_type;
+	(void)value;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -109,6 +130,10 @@ PowerManager::GetFanSpeedInfo(
 		uint32_t &rpm_min,
 		uint32_t &rpm_max,
 		uint32_t &rpm_step) {
+	(void)rp;
+	(void)rpm_min;
+	(void)rpm_max;
+	(void)rpm_step;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -117,17 +142,23 @@ PowerManager::SetFanSpeed(
 		ResourcePathPtr_t const & rp,
 		FanSpeedType fs_type,
 		uint32_t value) {
+	(void)rp;
+	(void)fs_type;
+	(void)value;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 PowerManager::PMResult
 PowerManager::ResetFanSpeed(ResourcePathPtr_t const & rp) {
+	(void)rp;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 
 PowerManager::PMResult
 PowerManager::GetPowerUsage(ResourcePathPtr_t const & rp, uint32_t &mwatt) {
+	(void)rp;
+	(void)mwatt;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -136,11 +167,16 @@ PowerManager::GetPowerInfo(
 		ResourcePathPtr_t const & rp,
 		uint32_t &mwatt_min,
 		uint32_t &mwatt_max) {
+	(void)rp;
+	(void)mwatt_min;
+	(void)mwatt_max;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 PowerManager::PMResult
 PowerManager::GetPowerState(ResourcePathPtr_t const & rp, int &state) {
+	(void)rp;
+	(void)state;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
@@ -148,27 +184,39 @@ PowerManager::PMResult
 PowerManager::GetPowerStatesInfo(
 	ResourcePathPtr_t const & rp,
 	int & min, int & max, int & step) {
+	(void)rp;
+	(void)min;
+	(void)max;
+	(void)step;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 PowerManager::PMResult
 PowerManager::SetPowerState(ResourcePathPtr_t const & rp, int state) {
+	(void)rp;
+	(void)state;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 
 PowerManager::PMResult
 PowerManager::GetPerformanceState(ResourcePathPtr_t const & rp, int &state) {
+	(void)rp;
+	(void)state;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 PowerManager::PMResult
 PowerManager::GetPerformanceStatesCount(ResourcePathPtr_t const & rp, int &count) {
+	(void)rp;
+	(void)count;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
 PowerManager::PMResult
 PowerManager::SetPerformanceState(ResourcePathPtr_t const & rp, int state) {
+	(void)rp;
+	(void)state;
 	return PMResult::ERR_API_NOT_SUPPORTED;
 }
 
