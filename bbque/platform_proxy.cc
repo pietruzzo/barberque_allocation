8050825ac3191514deb72024d3d391aa01ceea66
Giuseppe Massari <joe.massanga@gmail.com>
2014-10-17 14:43:13 +0200

    [PP] Power monitoring
    The Platform Proxy worker thread now can perform the monitoring of the
    power/thermal status of the hardware resources.
    
    To enable this feature, a suitable power management policy should register
    the set of resources to monitor with PowerMonitorRegister() and then
    call PowerMonitorStart(). The member function PowerMonitorSample() is in
    charge of collecting the status information required about the registered
    resources. PowerMonitorStop() stops the monitoring activity.
    
    Afterwards, the policy can retrieve the information collected from the
    Resource descriptors.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- bbque/platform_proxy.cc
+++ bbque/platform_proxy.cc
@@ -15,9 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <iomanip>
+#include <string>
+#include <sstream>
+
 #include "bbque/platform_proxy.h"
 #include "bbque/modules_factory.h"
 #include "bbque/resource_manager.h"
+#include "bbque/res/resource_path.h"
 #include "bbque/utils/utility.h"
 
 #ifdef CONFIG_BBQUE_TEST_PLATFORM_DATA
@@ -43,8 +48,14 @@ namespace bbque {
 
 PlatformProxy::PlatformProxy() : Worker(),
 	pilInitialized(false),
+#ifndef CONFIG_BBQUE_PM
 	platformIdentifier(NULL) {
+#else
+	platformIdentifier(NULL),
+	pm(PowerManager::GetInstance()) {
 
+	InitPowerMonitor();
+#endif
 	//---------- Setup Worker
 	Worker::Setup(BBQUE_MODULE_NAME("pp"), PLATFORM_PROXY_NAMESPACE);
 
@@ -62,6 +73,7 @@ PlatformProxy & PlatformProxy::GetInstance() {
 	static PLATFORM_PROXY instance;
 	return instance;
 }
+
 
 void PlatformProxy::Refresh() {
 	std::unique_lock<std::mutex> worker_status_ul(worker_status_mtx);
@@ -85,13 +97,22 @@ void PlatformProxy::Task() {
 		// Refresh available resources
 		if (platformEvents.test(PLATFORM_EVT_REFRESH))
 			RefreshPlatformData();
+
+#ifdef CONFIG_BBQUE_PM
+		// Monitor the power thermal status
+		if (platformEvents.test(PLATFORM_EVT_MONITOR)) {
+			PowerMonitorSample();
+			std::this_thread::sleep_for(
+					std::chrono::milliseconds(powerMonitorInfo.period_ms));
+		}
+#endif // CONFIG_BBQUE_PM
 	}
 
 	logger->Info("PLAT PRX: Monitoring thread ENDED");
 #else
 	logger->Info("PLAT PRX: Terminating monitoring thread (TPD in use)");
 	return;
-#endif
+#endif // CONFIG_BBQUE_TEST_PLATFORM_DATA
 
 }
 
@@ -170,6 +191,120 @@ PlatformProxy::CommitRefresh() {
 
 	return OK;
 }
+
+
+#ifdef CONFIG_BBQUE_PM
+
+void PlatformProxy::InitPowerMonitor() {
+	PowerMonitorGet[size_t(PowerManager::InfoType::LOAD)]        =
+		&PowerManager::GetLoad;
+	PowerMonitorGet[size_t(PowerManager::InfoType::TEMPERATURE)] =
+		&PowerManager::GetTemperature;
+	PowerMonitorGet[size_t(PowerManager::InfoType::FREQUENCY)]   =
+		&PowerManager::GetClockFrequency;
+	PowerMonitorGet[size_t(PowerManager::InfoType::POWER)]       =
+		&PowerManager::GetPowerUsage;
+	PowerMonitorGet[size_t(PowerManager::InfoType::PERF_STATE)]  =
+		&PowerManager::GetPerformanceState;
+	PowerMonitorGet[size_t(PowerManager::InfoType::POWER_STATE)] =
+		&PowerManager::GetPowerState;
+}
+
+PlatformProxy::ExitCode_t
+PlatformProxy::PowerMonitorRegister(
+		br::ResourcePathPtr_t rp,
+		PowerManager::SamplesArray_t const & samples_window) {
+	ResourceAccounter & ra(ResourceAccounter::GetInstance());
+
+	// Register all the resources referenced by the path specified
+	br::ResourcePtrList_t r_list(ra.GetResources(rp));
+	if (r_list.empty()) {
+		logger->Warn("No resources to monitor [%s]", rp->ToString().c_str());
+		return PLATFORM_PWR_MONITOR_ERROR;
+	}
+
+	// Specifiy the number of samples for the exponential mean computation
+	for (br::ResourcePtr_t rsrc: r_list) {
+			rsrc->EnablePowerProfile(samples_window);
+			powerMonitorInfo.resources.insert(
+					std::pair<br::ResourcePathPtr_t, br::ResourcePtr_t>(
+						ra.GetPath(rsrc->Path()), rsrc));
+	}
+
+	return PlatformProxy::OK;
+}
+
+void PlatformProxy::PowerMonitorStart(uint32_t period_ms) {
+	std::unique_lock<std::mutex> worker_status_ul(worker_status_mtx);
+	if (period_ms != 0)
+		powerMonitorInfo.period_ms = period_ms;
+	logger->Info("Starting power monitoring (T = %d ms)...",
+			powerMonitorInfo.period_ms);
+	platformEvents.set(PLATFORM_EVT_MONITOR);
+	worker_status_cv.notify_one();
+}
+
+void PlatformProxy::PowerMonitorStop() {
+	logger->Info("Stopping power monitoring...");
+	std::unique_lock<std::mutex> worker_status_ul(worker_status_mtx);
+	platformEvents.reset(PLATFORM_EVT_MONITOR);
+}
+
+PlatformProxy::ExitCode_t
+PlatformProxy::PowerMonitorSample() {
+	PowerManager::SamplesArray_t samples;
+	PowerManager::InfoType info_type;
+
+	// Power status monitoring over all the registered resources
+	for (auto & r_entry: powerMonitorInfo.resources) {
+		br::ResourcePathPtr_t const & r_path(r_entry.first);
+		br::ResourcePtr_t rsrc(r_entry.second);
+
+		std::string log_inst_values("Monitoring [");
+		std::string log_mean_values("Monitoring [");
+		log_inst_values += rsrc->Path() + "] (I): ";
+		log_mean_values += rsrc->Path() + "] (M): ";
+		uint info_idx   = 0;
+		uint info_count = 0;
+
+		for (; info_idx < PowerManager::InfoTypeIndex.size() &&
+				info_count < rsrc->GetPowerInfoEnabledCount();
+					++info_idx, ++info_count) {
+			// Check if the power profile information has been required
+			info_type = PowerManager::InfoTypeIndex[info_idx];
+			if (rsrc->GetPowerInfoSamplesWindowSize(info_type) <= 0)
+				continue;
+
+			// Call power manager get function and update the resource
+			// descriptor power profile information
+			(pm.*(PMfunc) PowerMonitorGet[info_idx])(r_path, samples[info_idx]);
+			rsrc->UpdatePowerInfo(info_type, samples[info_idx]);
+
+			// Log messages
+			std::stringstream ss_i;
+			ss_i
+				<< std::setprecision(0)       << std::fixed
+				<< std::setw(str_w[info_idx]) << std::left
+				<< rsrc->GetPowerInfo(info_type, br::Resource::INSTANT);
+			log_inst_values += ss_i.str() + " ";
+
+			std::stringstream ss_m;
+			ss_m
+				<< std::setprecision(str_p[info_idx]) << std::fixed
+				<< std::setw(str_w[info_idx])         << std::left
+				<< rsrc->GetPowerInfo(info_type, br::Resource::MEAN);
+			log_mean_values += ss_m.str() + " ";
+		}
+
+		logger->Debug("PLAT PRX: %s ", log_inst_values.c_str());
+		logger->Debug("PLAT PRX: %s ", log_mean_values.c_str());
+	}
+
+	return PlatformProxy::OK;
+}
+
+#endif // CONFIG_BBQUE_PM
+
 
 PlatformProxy::ExitCode_t
 PlatformProxy::Setup(AppPtr_t papp) {
