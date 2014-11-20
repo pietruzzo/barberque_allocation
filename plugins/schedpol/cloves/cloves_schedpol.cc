e8272274a33ffe5e2ec977417ce4ee4adb9572ee
Giuseppe Massari <joe.massanga@gmail.com>
2014-11-20 17:25:53 +0100

    [SchedPol_CLoves] First version
    The policy focuses OpenCL workloads on heterogeneous systems.
    To properly work it requires that:
      - The periodic mode, i.e. that the ResourceManager.opt_interval option
        in the configuration file could be set
      - The power monitoring is enabled (both CPU and GPU), such that is could
        be possible to get the current load level of the devices
      - The OpenCL application profiling is enabled (BBQUE_RTLIB_OPTS="o"), to
        provide runtime statistics about time spent to execute on the device,
        and time spent for memory data transfers.
    
    The current version works as follows:
    1) Profiling information are stored at AWM level, such that the policy
    could evaluated the best AWM, considering the Value and the runtime
    profiling. If an application provides and AWM for the execution of kernels
    on GPU, and another for the execution on CPU, this means to estimate the
    "best" device type for executing the application kernels.
    2) Given the device type chosen, if there are multiple devices available,
    the one with the lowest load level is picked
    3) The application is enqueued for the execution on the device into a
    priority queue. The priority is a metrics that takes into account how much
    the application has been served by the assignment of the AWM selected. In
    other words, higher priority is given to application that used the device
    for a less amount of time, weighting this time value with the AWM value.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ plugins/schedpol/cloves/cloves_schedpol.cc
@@ -0,0 +1,468 @@
+/*
+ * Copyright (C) 2014  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cloves_schedpol.h"
+
+#include <cstdlib>
+#include <cstdint>
+#include <iostream>
+
+#include "bbque/modules_factory.h"
+#include "bbque/utils/logging/logger.h"
+
+#include "bbque/application_manager.h"
+#include "bbque/app/working_mode.h"
+#include "bbque/res/binder.h"
+#include "bbque/res/resource_path.h"
+
+#define MODULE_CONFIG SCHEDULER_POLICY_CONFIG "." SCHEDULER_POLICY_NAME
+
+namespace bu = bbque::utils;
+namespace po = boost::program_options;
+
+namespace bbque { namespace plugins {
+
+// :::::::::::::::::::::: Static plugin interface ::::::::::::::::::::::::::::
+
+void * ClovesSchedPol::Create(PF_ObjectParams *) {
+	return new ClovesSchedPol();
+}
+
+int32_t ClovesSchedPol::Destroy(void * plugin) {
+	if (!plugin)
+		return -1;
+	delete (ClovesSchedPol *)plugin;
+	return 0;
+}
+
+// ::::::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
+
+char const * ClovesSchedPol::Name() {
+	return SCHEDULER_POLICY_NAME;
+}
+
+ClovesSchedPol::ClovesSchedPol():
+		cm(ConfigurationManager::GetInstance()),
+		ra(ResourceAccounter::GetInstance()),
+		pp(PlatformProxy::GetInstance()) {
+	// Logger instance
+	logger = bu::Logger::GetLogger(MODULE_NAMESPACE);
+	assert(logger);
+
+	if (logger)
+		logger->Info("cloves: Built a new dynamic object[%p]", this);
+	else
+		fprintf(stderr,
+				FI("cloves: Built new dynamic object [%p]\n"), (void *)this);
+}
+
+
+ClovesSchedPol::~ClovesSchedPol() {
+	queues.clear();
+}
+
+
+ClovesSchedPol::ExitCode_t ClovesSchedPol::Init() {
+	ExitCode_t result = OK;
+	ApplicationManager & am(ApplicationManager::GetInstance());
+
+	// Resource state view
+	result = InitResourceStateView();
+	if (result != OK) {
+		logger->Fatal("Init: Cannot get a resource state view");
+		return result;
+	}
+
+	// Device queues
+	if (!queues_ready) {
+		result = InitDeviceQueues();
+		if (result != OK) {
+			logger->Fatal("Init: Scheduling queues not available");
+			return result;
+		}
+		// Start sampling the devices status
+		pp.PowerMonitorStart(BBQUE_SP_CLOVES_SAMPLING_TIME);
+	}
+
+	// Update applications runtime profiling data
+	am.UpdateRuntimeProfiles();
+
+	return OK;
+}
+
+
+ClovesSchedPol::ExitCode_t ClovesSchedPol::InitResourceStateView() {
+	ResourceAccounterStatusIF::ExitCode_t ra_result;
+	ExitCode_t result = OK;
+
+	// Build a string path for the resource state view
+	++count;
+	snprintf(token_path, 30, "%s%d", MODULE_NAMESPACE, count);
+
+	// Get a fresh resource status view
+	logger->Debug("Init: Require a new resource state view [%s]", token_path);
+	ra_result = ra.GetView(token_path, sched_status_view);
+	if (ra_result != ResourceAccounterStatusIF::RA_SUCCESS)
+		return ERROR_VIEW;
+	logger->Info("Init: Resources state view token: %d", sched_status_view);
+
+	return result;
+}
+
+ClovesSchedPol::ExitCode_t ClovesSchedPol::InitDeviceQueues() {
+	ExitCode_t result = OK;
+	logger->Debug("Init: device queues initialization...");
+
+	// A device queue must be created for each binding domain
+	// (i.e., OpenCL device type (CPU, GPU,...))
+	BindingMap_t & bindings(ra.GetBindingOptions());
+	for (auto & bd_entry: bindings) {
+		br::Resource::Type_t  bd_type = bd_entry.first;
+		BindingInfo_t const & bd_info(*(bd_entry.second));
+
+		// Device map
+		DeviceQueueMapPtr_t pdev_queue_map = DeviceQueueMapPtr_t(new DeviceQueueMap_t());
+		queues.insert(
+			std::pair<br::ResourceIdentifier::Type_t, DeviceQueueMapPtr_t>(
+				bd_type, pdev_queue_map));
+		logger->Debug("Init: adding queues for device type '%s'",
+			br::ResourceIdentifier::TypeStr[bd_type]);
+
+		// Fill the map with device queues
+		CreateDeviceQueues(pdev_queue_map, bd_info);
+	}
+
+	logger->Info("Init: found %d device types", queues.size());
+	if (queues.empty())
+		result = ERROR_INIT;
+
+	queues_ready = true;
+	return result;
+}
+
+void ClovesSchedPol::CreateDeviceQueues(
+		DeviceQueueMapPtr_t & pdev_queue_map,
+		BindingInfo_t const & bd_info) {
+
+	// [CPU|GPU|..]0..n
+	for (br::ResourcePtr_t const & rsrc: bd_info.rsrcs) {
+		DeviceQueuePtr_t pdev_queue = DeviceQueuePtr_t(new DeviceQueue_t());
+
+		br::ResourcePathPtr_t r_path(new br::ResourcePath(rsrc->Path()));
+		r_path->AppendString("pe");
+
+		pdev_queue_map->insert(
+			std::pair<br::ResourcePathPtr_t, DeviceQueuePtr_t>(
+				r_path, pdev_queue));
+		logger->Debug("Init: added queue for '%s'",
+			r_path->ToString().c_str());
+	}
+
+	logger->Info("Init: added %d device queues for device type %s",
+		pdev_queue_map->size(), bd_info.d_path->ToString().c_str());
+}
+
+
+SchedulerPolicyIF::ExitCode_t
+ClovesSchedPol::Schedule(
+		System & system,
+		RViewToken_t & status_view) {
+	ExitCode_t result;
+
+	// Class providing query functions for applications and resources
+	sys = &system;
+
+	// Initialize a new resources state view
+	result = Init();
+	if (result != OK) {
+		logger->Fatal("Schedule: Initialization failed");
+		goto error;
+	}
+
+	// Iteration per priority
+	for (ba::AppPrio_t prio = 0;
+		prio <= sys->ApplicationLowestPriority(); ++prio) {
+		if (!sys->HasApplications(prio))
+			continue;
+		result = SchedulePriority(prio);
+	}
+
+	// Flush device quees sending the schedule requests
+	result = Flush();
+	if (result != OK) {
+		logger->Fatal("Schedule: Failures in schedule requests");
+		goto error;
+	}
+
+	// Return the new resource status view according to the new resource
+	// allocation performed
+	status_view = sched_status_view;
+	ra.PrintStatusReport(status_view);
+	return SCHED_DONE;
+error:
+	ra.PutView(status_view);
+	return SCHED_ERROR;
+}
+
+ClovesSchedPol::ExitCode_t
+ClovesSchedPol::SchedulePriority(ba::AppPrio_t prio) {
+	ExitCode_t result = OK;
+	AppsUidMapIt app_it;
+	ba::AppCPtr_t papp;
+
+	// For each application look for the best OpenCL device
+	papp = sys->GetFirstWithPrio(prio, app_it);
+	for (; papp; papp = sys->GetNextWithPrio(prio, app_it)) {
+		result = EnqueueIntoDevice(papp);
+	}
+	return result;
+}
+
+ClovesSchedPol::ExitCode_t
+ClovesSchedPol::EnqueueIntoDevice(ba::AppCPtr_t papp) {
+	br::ResourceIdentifier::Type_t dev_type = br::Resource::UNDEFINED;
+	float highest_xm_time_ratio = -1.0;
+	float xm_time_ratio;
+	uint64_t cpu_qt, gpu_qt;
+
+	// Device type selection: AWM evaluation
+	SchedEntityPtr_t psched(new SchedEntity_t(papp, nullptr, R_ID_NONE, 0.0));
+	ba::AwmPtrList_t const & awms(papp->WorkingModes());
+	logger->Debug("EnqueueIntoDevice: [%s], #AWMs: %d",
+		papp->StrId(), awms.size());
+	for (ba::AwmPtr_t const & pawm: awms) {
+		ba::WorkingMode::RuntimeProfiling_t awm_prof =
+			pawm->GetProfilingData();
+
+		// Execution time / Memory transfers time ratio
+		if ((awm_prof.mem_time == 0) || (awm_prof.exec_time == 0)) {
+			logger->Debug("EnqueueIntoDevice: [%s %s] profiling not available",
+				papp->StrId(), pawm->StrId());
+			xm_time_ratio = pawm->Value();
+		}
+		else {
+			logger->Debug("EnqueueIntoDevice: [%s %s] Xtime: %d, Mtime: %d",
+				papp->StrId(), pawm->StrId(),
+				awm_prof.exec_time, awm_prof.mem_time);
+			xm_time_ratio =
+				pawm->Value() *
+				(awm_prof.exec_time / awm_prof.mem_time);
+		}
+		logger->Debug("EnqueueIntoDevice: [%s %s] X/M time weighted ratio = %2.2f",
+			papp->StrId(), pawm->StrId(), xm_time_ratio);
+
+		if ((highest_xm_time_ratio > 0) &&
+			(xm_time_ratio <= highest_xm_time_ratio)) {
+			logger->Fatal("No update: h=%2.2f r=%2.2f",
+				highest_xm_time_ratio, xm_time_ratio);
+			continue;
+		}
+
+		// Set device type
+		gpu_qt = ra.GetUsageAmount(
+				pawm->RecipeResourceUsages(),
+				br::Resource::PROC_ELEMENT, br::Resource::GPU);
+		cpu_qt = ra.GetUsageAmount(
+				pawm->RecipeResourceUsages(),
+				br::Resource::PROC_ELEMENT, br::Resource::CPU);
+
+		gpu_qt > 0 ? dev_type = br::Resource::GPU: dev_type = br::Resource::CPU;
+		logger->Debug("EnqueueIntoDevice: [%s %s] requiring processing load: "
+				"GPU: %" PRIu64 ", CPU: %" PRIu64 "",
+				papp->StrId(), pawm->StrId(), gpu_qt, cpu_qt);
+
+		// AWM (device) selected so far
+		psched->SetAWM(pawm);
+		highest_xm_time_ratio = xm_time_ratio;
+	}
+
+	// Enqueing into the best device
+	logger->Debug("EnqueueIntoDevice: %s enqueing...", psched->StrId());
+	return Enqueue(psched, dev_type);
+}
+
+ClovesSchedPol::ExitCode_t
+ClovesSchedPol::Enqueue(
+		SchedEntityPtr_t psched,
+		br::ResourceIdentifier::Type_t dev_type) {
+	ExitCode_t result;
+
+	// Queue ordering metrics
+	ComputeOrderingMetrics(psched);
+
+	// Device queue
+	DeviceQueuePtr_t pdev_queue(SelectDeviceQueue(psched, dev_type));
+	if (pdev_queue == nullptr) {
+		logger->Fatal("Enqueue: device queue missing");
+		return ERROR_QUEUE;
+	}
+
+	// Resource binding
+	result = BindResources(psched);
+	if (result != OK) return result;
+	if (psched->bind_type == br::ResourceIdentifier::GPU) {
+		logger->Debug("Enqueue: %s binding host resources on CPU",
+			psched->StrId());
+		size_t b_refn = psched->pawm->BindResource(
+					br::ResourceIdentifier::CPU,
+					R_ID_ANY, R_ID_NONE,
+					psched->bind_refn);
+		psched->bind_refn = b_refn;
+	}
+
+	// Enqueue
+	pdev_queue->push(psched);
+	logger->Debug("Enqueue: %s queued [size: %d]",
+		psched->StrId(), pdev_queue->size());
+
+	return OK;
+}
+
+void ClovesSchedPol::ComputeOrderingMetrics(SchedEntityPtr_t psched) {
+	ba::WorkingMode::RuntimeProfiling_t const & awm_prof(
+			psched->pawm->GetProfilingData());
+	// Check the availability cumulated runtime profiling data
+	if ((awm_prof.exec_time_tot) == 0 ||
+			(psched->pawm->GetSchedulingCount() ==0)) {
+		psched->metrics = psched->pawm->Value();
+		logger->Debug("Order: %s profiling history not available yet",
+			psched->StrId());
+		return;
+	}
+
+	// Ordering metrics for the AWM in the device queue
+	psched->metrics =
+		awm_prof.exec_time_tot /
+		psched->pawm->GetSchedulingCount() * psched->pawm->Value();
+	logger->Info("Order: %s metrics = %2.2f [sched_count: %d, x_tot: %d]",
+		psched->StrId(), psched->metrics,
+		psched->pawm->GetSchedulingCount(), awm_prof.exec_time_tot);
+}
+
+ClovesSchedPol::DeviceQueuePtr_t
+ClovesSchedPol::SelectDeviceQueue(
+		SchedEntityPtr_t psched,
+		br::ResourceIdentifier::Type_t dev_type) {
+	br::ResourcePathPtr_t curr_path;
+	double min_load = 101.0;
+	double curr_load;
+
+	// Device type queue
+	DeviceQueueMapPtr_t & pdev_queue_map(queues[dev_type]);
+	DeviceQueueMap_t & dev_queue_map(*(pdev_queue_map.get()));
+
+	// Retrieve the queue of the device to bind
+	for (auto & dq_entry: dev_queue_map) {
+		br::ResourcePtrList_t const & r_list(ra.GetResources(dq_entry.first));
+		br::ResourcePtr_t const & rsrc(r_list.front());
+
+		if (rsrc == nullptr) {
+			logger->Fatal("Missing resource %s",
+				dq_entry.first->ToString().c_str());
+			return DeviceQueuePtr_t();
+		}
+
+		// Look for the device with the lowest load
+		curr_load = rsrc->GetPowerInfo(PowerManager::InfoType::LOAD);
+		if (curr_load < min_load) {
+			min_load  = curr_load;
+			curr_path = dq_entry.first;
+			if (dev_type == br::Resource::GPU)
+				psched->SetBindingID(
+					dq_entry.first->GetID(dev_type), dev_type);
+			else
+				psched->SetBindingID(R_ID_NONE, dev_type);
+		}
+		logger->Debug("SelectDeviceQueue: %s load = %2.1f",
+			rsrc->Path().c_str(), curr_load);
+	}
+
+	logger->Debug("SelectDeviceQueue: selected (%s)", curr_path->ToString().c_str());
+	return dev_queue_map[curr_path];
+}
+
+ClovesSchedPol::ExitCode_t
+ClovesSchedPol::BindResources(SchedEntityPtr_t psched) {
+	size_t r_refn;
+	r_refn = psched->pawm->BindResource(
+			psched->bind_type, R_ID_ANY,
+			psched->bind_id,
+			psched->bind_refn);
+	logger->Debug("BindResources: reference number %ld", r_refn);
+	if (r_refn == 0) {
+		logger->Error("BindResources: %s failed binding", psched->StrId());
+		return ERROR_RSRC;
+	}
+	psched->bind_refn = r_refn;
+
+	return OK;
+}
+
+
+ClovesSchedPol::ExitCode_t
+ClovesSchedPol::Flush() {
+	uint32_t sched_count = 0;
+
+	// Device types
+	for (auto & dtqm_entry: queues) {
+		DeviceQueueMap_t & dqm(*(dtqm_entry.second.get()));
+		// Devices
+		for (auto & dqm_entry: dqm) {
+			DeviceQueue_t & dev_queue(*dqm_entry.second.get());
+			sched_count += SendScheduleRequests(dev_queue);
+		}
+	}
+
+	if (sched_count == 0) return ERROR;
+	return OK;
+}
+
+uint32_t ClovesSchedPol::SendScheduleRequests(DeviceQueue_t & dev_queue) {
+	ba::Application::ExitCode_t app_result = ba::Application::APP_SUCCESS;
+	uint32_t sched_count = 0;
+
+	// Flush the entire device queue
+	while (!dev_queue.empty()) {
+
+		// Pick the topmost schedule entity
+		SchedEntityPtr_t const & psched(dev_queue.top());
+
+		// Skip if disabled meanwhile
+		if (psched->papp->Disabled()) {
+			logger->Debug("Flush: %s disabled. Skipping...");
+			dev_queue.pop();
+			continue;
+		}
+
+		// Send request
+		app_result = psched->papp->ScheduleRequest(
+			psched->pawm, sched_status_view, psched->bind_refn);
+		logger->Debug("Flush: %s schedule requested", psched->StrId());
+		if (app_result != ApplicationStatusIF::APP_WM_ACCEPTED) {
+			logger->Error("Flush: %s failed scheduling",
+				psched->StrId());
+		}
+		dev_queue.pop();
+		++sched_count;
+	}
+
+	return sched_count;
+}
+
+} // namespace plugins
+
+} // namespace bbque
---
+++ plugins/schedpol/cloves/cloves_schedpol.h
@@ -0,0 +1,273 @@
+/*
+ * Copyright (C) 2014  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BBQUE_CLOVES_SCHEDPOL_H_
+#define BBQUE_CLOVES_SCHEDPOL_H_
+
+#include <cstdint>
+#include <list>
+#include <memory>
+#include <map>
+#include <queue>
+
+#include "bbque/configuration_manager.h"
+#include "bbque/platform_proxy.h"
+#include "bbque/plugins/plugin.h"
+#include "bbque/plugins/scheduler_policy.h"
+#include "bbque/scheduler_manager.h"
+
+#define SCHEDULER_POLICY_NAME "cloves"
+
+#define MODULE_NAMESPACE SCHEDULER_POLICY_NAMESPACE "." SCHEDULER_POLICY_NAME
+
+#define BBQUE_SP_CLOVES_SAMPLING_TIME 200
+
+using bbque::res::RViewToken_t;
+using bbque::utils::MetricsCollector;
+using bbque::utils::Timer;
+
+// These are the parameters received by the PluginManager on create calls
+struct PF_ObjectParams;
+
+namespace bbque { namespace plugins {
+
+class LoggerIF;
+
+/**
+ * @class ClovesSchedPol
+ *
+ * This is the core of a scheduling policy plugin mainly targeting
+ * heterogeneous workloads, i.e. applications (like OpenCL ones) that can be
+ * executed on different computing devices
+ */
+class ClovesSchedPol: public SchedulerPolicyIF {
+
+public:
+
+	// :::::::::::::::::::::: Static plugin interface :::::::::::::::::::::::::
+
+	/**
+	 * @brief Create the cloves plugin
+	 */
+	static void * Create(PF_ObjectParams *);
+
+	/**
+	 * @brief Destroy the cloves plugin
+	 */
+	static int32_t Destroy(void *);
+
+
+	// :::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
+
+	/**
+	 * @brief Destructor
+	 */
+	virtual ~ClovesSchedPol();
+
+	/**
+	 * @brief Return the name of the policy plugin
+	 */
+	char const * Name();
+
+
+	/**
+	 * @brief The member function called by the SchedulerManager to perform a
+	 * new scheduling / resource allocation
+	 */
+	ExitCode_t Schedule(System & system, RViewToken_t & status_view);
+
+private:
+
+	/**
+	 * @brief Specific internal exit code of the class
+	 */
+	enum ExitCode_t {
+		OK,
+		ERROR,
+		ERROR_INIT,
+		ERROR_VIEW,
+		ERROR_QUEUE,
+		ERROR_RSRC
+	};
+
+	/** Shared pointer to a scheduling entity */
+	typedef std::shared_ptr<SchedEntity_t> SchedEntityPtr_t;
+
+
+	/** Queue of entity to schedule on the device */
+	typedef std::priority_queue<SchedEntityPtr_t> DeviceQueue_t;
+	typedef std::shared_ptr<DeviceQueue_t> DeviceQueuePtr_t;
+
+	/** Map of device queues with the resource path as key */
+	typedef std::map<br::ResourcePathPtr_t, DeviceQueuePtr_t> DeviceQueueMap_t;
+	typedef std::shared_ptr<DeviceQueueMap_t> DeviceQueueMapPtr_t;
+
+	/** Set of device queues for device type (e.g., CPU, GPU,..) */
+	typedef std::map<br::ResourceIdentifier::Type_t, DeviceQueueMapPtr_t> DeviceTypeQueueMap_t;
+
+
+	/** Configuration manager instance */
+	ConfigurationManager & cm;
+
+	/** Resource accounter instance */
+	ResourceAccounter & ra;
+
+	/** Platform Proxy instance */
+	PlatformProxy & pp;
+
+	/** System logger instance */
+	std::unique_ptr<bu::Logger> logger;
+
+	/** System view:
+	 *  This points to the class providing the functions to query information
+	 *  about applications and resources
+	 */
+	System * sys;
+
+
+	/** Reference to the current scheduling status view of the resources */
+	RViewToken_t sched_status_view;
+
+	/** String for requiring new resource status views */
+	char token_path[30];
+
+	/** A counter used for getting always a new clean resources view */
+	uint32_t count = 0;
+
+
+	/** Map of queues, one for device type */
+	DeviceTypeQueueMap_t queues;
+
+	bool queues_ready = false;
+
+
+	/** An High-Resolution timer */
+	Timer timer;
+
+	/**
+	 * @brief Constructor
+	 *
+	 * Plugins objects could be build only by using the "create" method.
+	 * Usually the PluginManager acts as object
+	 */
+	ClovesSchedPol();
+
+	/**
+	 * @brief Optional initialization member function
+	 */
+	ExitCode_t Init();
+
+	/**
+	 * @brief Get a clean resource status view
+	 */
+	ExitCode_t InitResourceStateView();
+
+	/**
+	 * @brief Initialize the device queuesw
+	 */
+	ExitCode_t InitDeviceQueues();
+
+	/**
+	 * @brief Device queues allocation
+	 *
+	 * @param pdev_queue_map The map of resource path/device queue
+	 * @param bd_info Support information for the resource binding
+	 */
+	void CreateDeviceQueues(
+			DeviceQueueMapPtr_t & pdev_queue_map,
+			BindingInfo_t const & bd_info);
+
+	/**
+	 * @brief Scheduling of the applications/EXCs with a given priority
+	 * value
+	 *
+	 * @param prio The priority value
+	 * @return
+	 */
+	ExitCode_t SchedulePriority(ba::AppPrio_t prio);
+
+	/**
+	 * @brief Evaluate the scheduling of the applications/EXCs
+	 *
+	 * @param papp The application/EXC to schedule
+	 * @return
+	 */
+	ExitCode_t EnqueueIntoDevice(ba::AppCPtr_t papp);
+
+	/**
+	 * @brief Enqueue a scheduling entity into a device of the given
+	 * resource type
+	 *
+	 * @param psched The schedule entity to enqueue
+	 * @param dev_type The device type (e.g. CPU, GPU)
+	 *
+	 * @return
+	 */
+	ExitCode_t Enqueue(
+		SchedEntityPtr_t psched,
+		br::ResourceIdentifier::Type_t dev_type);
+
+	/**
+	 * @brief Compute the metrics used for the ordering of the scheduling
+	 * entity into the device queue resource type
+	 *
+	 * @param psched The schedule entity to enqueue
+	 */
+	void ComputeOrderingMetrics(SchedEntityPtr_t psched);
+
+	/**
+	 * @brief Select the best device queue of the given resource type, for
+	 * the scheduling entity provided entity into the device queue resource
+	 *
+	 * @param dev_type The device type (e.g. CPU, GPU)
+	 * @param psched The schedule entity to enqueue
+	 *
+	 * @return A pointer to the device queue
+	 */
+	DeviceQueuePtr_t SelectDeviceQueue(
+		SchedEntityPtr_t psched,
+		br::ResourceIdentifier::Type_t dev_type);
+
+	/**
+	 * @brief Bind the resource path of the AWM, coming from the recipe
+	 *
+	 * @param psched The schedule entity to enqueue
+	 */
+	ExitCode_t BindResources(SchedEntityPtr_t psched);
+
+	/**
+	 * @brief Flush the device queues, proceeding with the scheduling
+	 * requests
+	 */
+	ExitCode_t Flush();
+
+	/**
+	 * @brief Send the scheduling requests for each of the application/EXC
+	 * in the queue requests
+	 *
+	 * @param dev_queue The device queue
+	 *
+	 * @return The number of successful requests
+	 */
+	uint32_t SendScheduleRequests(DeviceQueue_t & dev_queue);
+};
+
+} // namespace plugins
+
+} // namespace bbque
+
+#endif // BBQUE_CLOVES_SCHEDPOL_H_
