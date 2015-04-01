72dbae79986a2e3a762bb61c5853543128b6c4b5
Giuseppe Massari <joe.massanga@gmail.com>
2015-04-01 19:33:08 +0200

    [ODROIDXU_PM] Root header file
    The header file is supposed to include common definitions of variables,
    macros, settings, etc... for ODROID-XU platforms.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ include/bbque/pm/power_manager_odroidxu.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef BBQUE_POWER_MANAGER_ODROID_H_
+#define BBQUE_POWER_MANAGER_ODROID_H_
+
+#include "bbque/config.h"
+
+namespace bbque {
+
+/***********************************************
+ * Odroid XU-3 board
+ ***********************************************/
+#ifdef CONFIG_TARGET_ODROID_XU
+
+#define  BBQUE_ODROID_CPU_CORES_NUM    8
+
+// Drivers directory
+#define BBQUE_ODROID_SYS_DIR_GPU\
+	"/sys/bus/platform/drivers/mali/11800000.mali/"
+// Sensors
+#define BBQUE_ODROID_SENSORS_DIR_A7    "/sys/bus/i2c/drivers/INA231/3-0041/"
+#define BBQUE_ODROID_SENSORS_DIR_A15   "/sys/bus/i2c/drivers/INA231/3-0040/"
+#define BBQUE_ODROID_SENSORS_DIR_GPU   "/sys/bus/i2c/drivers/INA231/3-0044/"
+#define BBQUE_ODROID_SENSORS_DIR_MEM   "/sys/bus/i2c/drivers/INA231/3-0045/"
+// Attributes
+#define BBQUE_ODROID_SENSORS_ATT_V     "sensor_V"
+#define BBQUE_ODROID_SENSORS_ATT_W     "sensor_W"
+#define BBQUE_ODROID_SENSORS_ATT_A     "sensor_A"
+
+#endif // TARGET_ODROID_XU
+
+/***********************************************
+ * Odroid XU-E board
+ ***********************************************/
+
+} // namespace bbque
+
+#endif // BBQUE_POWER_MANAGER_ODROID_H_
