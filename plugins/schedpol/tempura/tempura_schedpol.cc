44eba267f5713451932189b843b90abc96c5a2b6
Giuseppe Massari <joe.massanga@gmail.com>
2015-04-22 19:57:04 +0200

    [SchedPol_Tempura] Baseline implementation
    The basic idea of the policy is that starting from thermal and/or energy
    constraints, the amount of resource (budget) to allocate is defined
    accordingly.
    
    Given all the applications and their priorities, the number of "slots" is
    computed. In this way, the resource budget is converted into slots, such
    that the policy could assign the amount of resources proportional to the
    application priority.
    
    This is the first policy that builds an AWM online instead of iterating
    over the list coming from the Recipe.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- plugins/schedpol/tempura/tempura_schedpol.cc
+++ plugins/schedpol/tempura/tempura_schedpol.cc
@@ -26,9 +26,13 @@
 
 #include "bbque/app/working_mode.h"
 #include "bbque/res/binder.h"
+#include "bbque/res/identifier.h"
+#include "bbque/res/usage.h"
+#include "bbque/res/resource_path.h"
 
 #define MODULE_CONFIG SCHEDULER_POLICY_CONFIG "." SCHEDULER_POLICY_NAME
 
+namespace br = bbque::res;
 namespace bu = bbque::utils;
 namespace po = boost::program_options;
 
@@ -73,42 +77,326 @@ TempuraSchedPol::~TempuraSchedPol() {
 
 }
 
-TempuraSchedPol::ExitCode_t TempuraSchedPol::Init() {
-	ResourceAccounterStatusIF::ExitCode_t ra_result;
-	ExitCode_t result = OK;
+SchedulerPolicyIF::ExitCode_t TempuraSchedPol::Init() {
+	ExitCode_t result = SCHED_OK;
 
-	// Build a string path for the resource state view
-	char token_path[30];
-	++status_view_count;
-	snprintf(token_path, strlen(token_path), "%s%d",
-			MODULE_NAMESPACE, status_view_count);
-
-	// Get a fresh resource status view
-	logger->Debug("Init: Require a new resource state view [%s]", token_path);
-	ra_result = ra.GetView(token_path, sched_status_view);
-	if (ra_result != ResourceAccounterStatusIF::RA_SUCCESS) {
+	// Resource state view
+	result = InitResourceStateView();
+	if (result != SCHED_OK) {
 		logger->Fatal("Init: Cannot get a resource state view");
-		return ERROR_VIEW;
+		return result;
 	}
-	logger->Debug("Init: Resources state view token: %d", sched_status_view);
+
+	// Application slots
+	InitSlots();
+
+	// Power budgets data structures
+	if (!power_budgets.empty()) {
+		logger->Debug("Init: Power budgets initialized");
+		return SCHED_OK;
+	}
+
+	result = InitBudgets();
+	if (result != SCHED_OK) {
+		logger->Fatal("Init: Power budgets initialization failed");
+		return result;
+	}
 
 	return result;
+}
+
+SchedulerPolicyIF::ExitCode_t
+TempuraSchedPol::InitResourceStateView() {
+	ResourceAccounterStatusIF::ExitCode_t ra_result;
+	ExitCode_t result = SCHED_OK;
+
+	// Build a string path for the resource state view
+	snprintf(status_view_id, 30, "%s%d", MODULE_NAMESPACE, ++sched_count);;
+
+	// Get a fresh resource status view
+	logger->Debug("Init: Require a new resource state view [%s]", status_view_id);
+	ra_result = ra.GetView(status_view_id, sched_status_view);
+	if (ra_result != ResourceAccounterStatusIF::RA_SUCCESS)
+		return SCHED_ERROR_VIEW;
+	logger->Info("Init: Resources state view token: %d", sched_status_view);
+
+	return result;
+}
+
+SchedulerPolicyIF::ExitCode_t
+TempuraSchedPol::InitBudgets() {
+
+	// Binding type (CPU, GPU,...))
+	BindingMap_t & bindings(ra.GetBindingOptions());
+	for (auto & bd_entry: bindings) {
+		BindingInfo_t const & bd_info(*(bd_entry.second));
+
+		// Resource path e.g., "sys0.cpu[0..n].XX"
+		for (br::ResourcePtr_t const & rsrc: bd_info.rsrcs) {
+			br::ResourcePathPtr_t r_path(new br::ResourcePath(rsrc->Path()));
+			r_path->AppendString("pe");
+			// Budget object (path + budget value)
+			br::ResourcePtrList_t r_list(ra.GetResources(r_path));
+			br::UsagePtr_t pbudget(new br::Usage(0));
+			br::UsagePtr_t rbudget(new br::Usage(0));
+			pbudget->SetResourcesList(r_list);
+			rbudget->SetResourcesList(r_list);
+
+			// Add to the power budgets map
+			power_budgets.insert(
+					std::pair<br::ResourcePathPtr_t, br::UsagePtr_t>(
+						r_path, pbudget));
+			// Add to the resource budgets map
+			resource_budgets.insert(
+					std::pair<br::ResourcePathPtr_t, br::UsagePtr_t>(
+						r_path, rbudget));
+			logger->Debug("Init: Budgeting on '%s'",
+					r_path->ToString().c_str());
+		}
+	}
+
+	return SCHED_OK;
+}
+
+SchedulerPolicyIF::ExitCode_t
+TempuraSchedPol::InitSlots() {
+	slots = 0;
+	for (AppPrio_t p = 0; p <= sys->ApplicationLowestPriority(); p++) {
+		slots += (sys->ApplicationLowestPriority()+1 - p) * sys->ApplicationsCount(p);
+		logger->Debug("Init: Slots for prio %d [c=%d] = %d",
+				p, sys->ApplicationsCount(p), slots);
+	}
+	logger->Info("Init: Slots for partitioning = %d", slots);
+
+	return SCHED_OK;
 }
 
 SchedulerPolicyIF::ExitCode_t
 TempuraSchedPol::Schedule(
 		System & system,
 		RViewToken_t & status_view) {
-	SchedulerPolicyIF::ExitCode_t result = SCHED_DONE;
-
-	// Class providing query functions for applications and resources
+	SchedulerPolicyIF::ExitCode_t result;
 	sys = &system;
+
+	result = Init();
+	if (result != SCHED_OK) {
+		logger->Fatal("Schedule: Policy initialization failed");
+		goto error;
+	}
+
+	result = ComputeBudgets();
+	if (result != SCHED_OK) {
+		logger->Fatal("Schedule: Budgets cannot be computed");
+		goto error;
+	}
+
+	result = DoResourcePartitioning();
+	if (result != SCHED_OK) {
+		logger->Fatal("Schedule: Resource partitioning failed");
+		goto error;
+	}
+
+	result = DoScheduling();
+	if (result != SCHED_OK) {
+		logger->Fatal("Schedule: Scheduling failed");
+		goto error;
+	}
 
 	// Return the new resource status view according to the new resource
 	// allocation performed
 	status_view = sched_status_view;
-	return result;
+	return SCHED_DONE;
+
+error:
+	ra.PutView(status_view);
+	return SCHED_ERROR;
+
 }
+
+SchedulerPolicyIF::ExitCode_t TempuraSchedPol::ComputeBudgets() {
+
+	for (auto & pb_entry: power_budgets) {
+		br::ResourcePathPtr_t const & r_path(pb_entry.first);
+		br::UsagePtr_t & budget(pb_entry.second);
+
+		uint32_t p_budget = GetPowerBudget(r_path);
+		budget->SetAmount(p_budget);
+
+		uint64_t r_budget = GetResourceBudget(r_path);
+		resource_budgets[r_path]->SetAmount(r_budget);
+		logger->Info("Budget: [%s] has a budget of %" PRIu64 "",
+				r_path->ToString().c_str(),
+				resource_budgets[r_path]->GetAmount());
+	}
+
+	return SCHED_OK;
+}
+
+uint32_t TempuraSchedPol::GetPowerBudget(br::ResourcePathPtr_t const & rp) {
+	(void) rp;
+	return 5;
+
+}
+
+int64_t TempuraSchedPol::GetResourceBudget(br::ResourcePathPtr_t const & rp) {
+	(void) rp;
+	return 80;
+
+}
+
+SchedulerPolicyIF::ExitCode_t TempuraSchedPol::DoResourcePartitioning() {
+	ba::AppCPtr_t papp;
+	AppsUidMapIt app_it;
+
+	// Ready applications
+	papp = sys->GetFirstReady(app_it);
+	for (; papp; papp = sys->GetNextReady(app_it)) {
+		AssignWorkingMode(papp);
+	}
+
+	// Running applications
+	papp = sys->GetFirstRunning(app_it);
+	for (; papp; papp = sys->GetNextRunning(app_it)) {
+		AssignWorkingMode(papp);
+	}
+
+	return SCHED_OK;
+}
+
+bool TempuraSchedPol::CheckSkip(ba::AppCPtr_t const & papp) {
+	if (!papp->Active() && !papp->Blocking()) {
+		logger->Debug("Skipping [%s] State = [%s, %s]",
+				papp->StrId(),
+				ApplicationStatusIF::StateStr(papp->State()),
+				ApplicationStatusIF::SyncStateStr(papp->SyncState()));
+		return true;
+	}
+
+	// Avoid double AWM selection for already RUNNING applications
+	if ((papp->State() == Application::RUNNING) && papp->NextAWM()) {
+		logger->Debug("Skipping [%s] AWM %d => No reconfiguration",
+				papp->StrId(), papp->CurrentAWM()->Id());
+		return true;
+	}
+
+	// Avoid double AWM selection for already SYNC applications
+	if ((papp->State() == Application::SYNC) && papp->NextAWM()) {
+		logger->Debug("Skipping [%s] AWM already assigned [%d]",
+				papp->StrId(), papp->NextAWM()->Id());
+		return true;
+	}
+
+	return false;
+}
+
+
+SchedulerPolicyIF::ExitCode_t
+TempuraSchedPol::AssignWorkingMode(ba::AppCPtr_t papp) {
+	uint64_t resource_slot;
+	uint64_t resource_amount;
+
+	// Build a new working mode featuring assigned resources
+	ba::AwmPtr_t pawm(
+			new ba::WorkingMode(
+				papp->WorkingModes().size(), "Run-time", 1));
+	pawm->SetOwner(papp);
+
+	// Resource assignment (from each binding domain)
+	for (auto & rb_entry: resource_budgets) {
+		br::ResourcePathPtr_t const & r_path(rb_entry.first);
+		br::UsagePtr_t & resource_budget(rb_entry.second);
+
+		// Slots to allocate for this resource binding domain
+		logger->Debug("Assign: Resource [%s] budget = % " PRIu64 "",
+				r_path->ToString().c_str(), resource_budget->GetAmount());
+		resource_slot  = resource_budget->GetAmount() / slots;
+		logger->Debug("Assign: [%s] rslots of [%s] assigned = %4d",
+				papp->StrId(), r_path->ToString().c_str(), resource_slot);
+		resource_amount =
+				(sys->ApplicationLowestPriority() - papp->Priority() + 1) *
+				resource_slot;
+		logger->Debug("Assign: [%s] amount of [%s] assigned = %4d",
+				papp->StrId(), r_path->ToString().c_str(), resource_amount);
+		pawm->AddResourceUsage(r_path->ToString(), resource_amount);
+	}
+
+	// Enqueue scheduling entity
+	SchedEntityPtr_t psched(new SchedEntity_t(papp, pawm, R_ID_ANY, 0));
+	entities.push_back(psched);
+
+	return SCHED_OK;
+}
+
+SchedulerPolicyIF::ExitCode_t TempuraSchedPol::DoScheduling() {
+	SchedulerPolicyIF::ExitCode_t result;
+	Application::ExitCode_t app_result = Application::APP_SUCCESS;
+	logger->Debug("DoScheduling: START");
+
+	for (SchedEntityPtr_t & psched: entities) {
+		// Bind the assigned resources and try to schedule
+		result = DoBinding(psched);
+		if (result != SCHED_OK) {
+			logger->Error("DoScheduling: [%s] skipping scheduling", psched->StrId());
+			continue;
+		}
+
+		// Check application status
+		if (CheckSkip(psched->papp)) {
+			logger->Debug("DoScheduling: [%s] skipping status", psched->StrId());
+			continue;
+		}
+
+		// Scheduling request
+		logger->Info("DoScheduling: [%s] scheduling request", psched->StrId());
+		app_result = psched->papp->ScheduleRequest(
+				psched->pawm, sched_status_view, psched->bind_refn);
+		if (app_result != ApplicationStatusIF::APP_WM_ACCEPTED) {
+			logger->Error("DoScheduling: [%s] failed", psched->StrId());
+			continue;
+		}
+		logger->Debug("DoScheduling: [%s] success", psched->StrId());
+	}
+	logger->Debug("DoScheduling: STOP");
+	return SCHED_OK;
+}
+
+SchedulerPolicyIF::ExitCode_t TempuraSchedPol::DoBinding(
+		SchedEntityPtr_t psched) {
+	logger->Debug("DoBinding: START");
+	size_t ref_n = 0;
+
+	BindingMap_t & bindings(ra.GetBindingOptions());
+	for (auto & bd_entry: bindings) {
+		BindingInfo_t const & bd_info(*(bd_entry.second));
+		br::ResourceIdentifier::Type_t bd_type = bd_entry.first;
+		// CPU, GPU level binding
+		// Resource path e.g., "sys0.cpu[0..n].XX"
+		for (br::ResourcePtr_t const & rsrc: bd_info.rsrcs) {
+			br::ResID_t bd_id = rsrc->ID();
+			logger->Debug("DoBinding: [%s] binding to %s%d",
+					psched->StrId(), br::ResourceIdentifier::TypeStr[bd_type], bd_id);
+
+			ref_n = psched->pawm->BindResource(bd_type, bd_id, bd_id, ref_n);
+			logger->Debug("DoBinding: [%s] reference number %ld",
+					psched->StrId(), ref_n);
+
+			// The resource binding should never fail
+			if (ref_n == 0) {
+				logger->Error("DoBinding: [%d] on '%s%d' failed",
+						psched->StrId(),
+						br::ResourceIdentifier::TypeStr[bd_type], bd_id);
+				return SCHED_ERROR;
+			}
+		}
+
+		// Set scheduled binding reference number
+		psched->bind_refn = ref_n;
+	}
+
+	logger->Debug("DoBinding: STOP");
+	return SCHED_OK;
+}
+
 
 } // namespace plugins
 
--- plugins/schedpol/tempura/tempura_schedpol.h
+++ plugins/schedpol/tempura/tempura_schedpol.h
@@ -26,6 +26,7 @@
 #include "bbque/plugins/plugin.h"
 #include "bbque/plugins/scheduler_policy.h"
 #include "bbque/scheduler_manager.h"
+#include "bbque/resource_manager.h"
 
 #define SCHEDULER_POLICY_NAME "tempura"
 
@@ -34,6 +35,8 @@
 using bbque::res::RViewToken_t;
 using bbque::utils::MetricsCollector;
 using bbque::utils::Timer;
+
+namespace br = bbque::res;
 
 // These are the parameters received by the PluginManager on create calls
 struct PF_ObjectParams;
@@ -59,7 +62,7 @@ public:
 	static void * Create(PF_ObjectParams *);
 
 	/**
-	 * @brief Destroy the tempura plugin 
+	 * @brief Destroy the tempura plugin
 	 */
 	static int32_t Destroy(void *);
 
@@ -84,15 +87,6 @@ public:
 	ExitCode_t Schedule(System & system, RViewToken_t & status_view);
 
 private:
-
-	/**
-	 * @brief Specific internal exit code of the class
-	 */
-	enum ExitCode_t {
-		OK,
-		ERROR,
-		ERROR_VIEW
-	};
 
 	/** Shared pointer to a scheduling entity */
 	typedef std::shared_ptr<SchedEntity_t> SchedEntityPtr_t;
@@ -120,11 +114,28 @@ private:
 	/** Reference to the current scheduling status view of the resources */
 	RViewToken_t sched_status_view;
 
+	/** Scheduler counter */
+	uint32_t sched_count = 0;
+
+	/** String for requiring new resource status views */
+	char status_view_id[30];
+
 	/** A counter used for getting always a new clean resources view */
 	uint32_t status_view_count = 0;
 
 	/** List of scheduling entities  */
 	SchedEntityList_t entities;
+
+	/** Allocatable resource slots */
+	uint32_t slots ;
+
+
+	/** Power budgets due to thermal or energy constraints */
+	br::UsagesMap_t power_budgets;
+
+	/** Resource budgets according to the power budgets */
+	br::UsagesMap_t resource_budgets;
+
 
 
 	/** An High-Resolution timer */
@@ -139,9 +150,100 @@ private:
 	TempuraSchedPol();
 
 	/**
-	 * @brief Optional initialization member function
+	 * @brief Policy initialization
 	 */
 	ExitCode_t Init();
+
+	/**
+	 * @brief Initialize the new resource status view
+	 */
+	ExitCode_t InitResourceStateView();
+
+	/**
+	 * @brief Initialize the power and resource budgets
+	 */
+	ExitCode_t InitBudgets();
+
+	/**
+	 * @brief Initialize the slots to allocate
+	 *
+	 * The slot is a resource unity of allocation. The idea is that a resource
+	 * budget can be seen as a number slots. The slots assigned to each
+	 * application are proportional to its priority
+	 */
+	ExitCode_t InitSlots();
+
+	/**
+	 * @brief Compute power and resource budgets
+	 *
+	 * This step is performed according to the constraints on the critical
+	 * termal threshold and (optionally) the energy budget
+	 */
+	ExitCode_t ComputeBudgets();
+
+	/**
+	 * @brief Define the power budget of a specific resource to allocate
+	 * (power capping)
+	 *
+	 * @param rp The resource path
+	 * @return The power value to cap (in milliwatts)
+	 */
+	uint32_t GetPowerBudget(br::ResourcePathPtr_t const & rp);
+
+	/**
+	 * @brief Define the resource budget to allocate according to the power
+	 * budget
+	 *
+	 * The function is in charge of computing the amount of resource to
+	 * allocate, for instance by capping the CPU total bandwith and/or setting
+	 * the CPU cores frequencies.
+	 *
+	 * @param rp The resource path
+	 * @return The total amount of allocatable resource
+	 */
+	int64_t GetResourceBudget(br::ResourcePathPtr_t const & rp);
+
+	/**
+	 * @brief Perform the resource partitioning among active applications
+	 *
+	 * @return SCHED_OK for success
+	 */
+	ExitCode_t DoResourcePartitioning();
+
+	/**
+	 * @brief Build the working mode of assigned resources
+	 *
+	 * @param papp Pointer to the application to schedule
+	 *
+	 * @return SCHED_OK for success
+	 */
+	ExitCode_t AssignWorkingMode(ba::AppCPtr_t papp);
+
+	/**
+	 * @brief Check if the application does not need to be re-scheduled
+	 *
+	 * @param papp The pointer to the application descriptor
+	 *
+	 * @return true if the application must be skipped, false otherwise
+	 */
+	bool CheckSkip(ba::AppCPtr_t const & papp);
+
+	/**
+	 * @brief Do resource binding and send scheduling requests
+	 *
+	 * @return SCHED_OK for success
+	 */
+	ExitCode_t DoScheduling();
+
+	/**
+	 * @brief Bind the working mode to platform resources
+	 *
+	 * @param papp Pointer to the scheduling entity (application and working
+	 * mode)
+	 *
+	 * @return SCHED_OK for success
+	 */
+	ExitCode_t DoBinding(SchedEntityPtr_t psched);
 };
 
 } // namespace plugins
