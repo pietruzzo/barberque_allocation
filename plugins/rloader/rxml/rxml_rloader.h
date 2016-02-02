c1ea555424207e27c2510536d83c4ecaace8e961
Giuseppe Massari <joe.massanga@gmail.com>
2016-02-02 12:06:17 +0100

    [RecipeLoader_RXML] Update header file position
    The installation path of the RapidXML library has been changed.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- plugins/rloader/rxml/rxml_rloader.h
+++ plugins/rloader/rxml/rxml_rloader.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014  Politecnico di Milano
+ * Copyright (C) 2015  Politecnico di Milano
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 
 #include "bbque/plugins/recipe_loader.h"
 
-#include <rapidxml.hpp>
+#include <rapidxml/rapidxml.hpp>
 
 #include "bbque/modules_factory.h"
 #include "bbque/plugin_manager.h"
