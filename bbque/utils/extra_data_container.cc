b333d0eeaa4d893b66f1be9fa45bb3571295e08f
Giuseppe Massari <joe.massanga@gmail.com>
2016-06-08 15:08:12 +0200

    [Utils_AC] Fixed wrong header inclusion
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- bbque/utils/extra_data_container.cc
+++ bbque/utils/extra_data_container.cc
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "bbque/utils/attributes_container.h"
+#include "bbque/utils/extra_data_container.h"
 #include "bbque/utils/utility.h"
 
 #include <iostream>
