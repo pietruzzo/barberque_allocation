5245099e483adcb7e5fe9e4291e2460a69496e6a
Giuseppe Massari <joe.massanga@gmail.com>
2016-07-20 18:46:54 +0200

    [Plugins] AgentProxy introduction
    This plugin must provide connectivity to the BarbequeRTRM such that it
    becomes possible to deploy the framework on distributed platforms. The
    goal is to enable the resource manager to operate in a multi-agent based
    configuration. This commit introduces the first definition of the plugin
    interface.
    
    An instance of BarbequeRTRM should be able to retrieve data from another
    instance (running on a different system node) about status of the resources, the
    current workload and the communication channel.
    
    Moreover, a further idea is that more instances (system nodes) can join or
    not the same partition. Accordingly a distributed system can be partitioned
    such that different subsets of nodes can be managed separately.
    
    Different mode will be defined for the distributed configuration. The
    simplest one is a hierarchical centralized approach, where the resource
    allocation policy is ran by a single (master) instance of the resource
    manager. As a consequence the AgentProxy must provide an interface to
    send a schedule request to a remote node.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ include/bbque/plugins/agent_proxy_if.h
@@ -0,0 +1,115 @@
+#ifndef BBQUE_REMOTE_PROXY_IF_H
+#define BBQUE_REMOTE_PROXY_IF_H
+
+#include "bbque/plugins/agent_proxy_types.h"
+
+namespace bbque
+{
+namespace plugins
+{
+
+using bbque::agent::ExitCode_t;
+
+/**
+ * @class AgentProxyIF
+ *
+ * @brief Interface for enabling the multi-agent configuration of
+ * the BarbequeRTRM
+ */
+class AgentProxyIF {
+
+public:
+
+	AgentProxyIF() {}
+
+	AgentProxyIF(const std::string & _port);
+
+	virtual ~AgentProxyIF() {}
+
+	virtual void WaitForServerToStop() = 0;
+
+	// ----------------- Query status functions --------------------
+
+	/**
+	 * @brief GetResourceStatus
+	 * @param path
+	 * @param status
+	 * @return
+	 */
+	virtual ExitCode_t GetResourceStatus(
+		std::string const & resource_path,
+		agent::ResourceStatus & status) = 0;
+
+	/**
+	 * @brief GetWorkloadStatus
+	 * @param path
+	 * @param status
+	 * @return
+	 */
+	virtual ExitCode_t GetWorkloadStatus(
+		std::string const & path, agent::WorkloadStatus & status) = 0;
+	/**
+	 * @param system_id
+	 */
+	virtual ExitCode_t GetWorkloadStatus(
+		int system_id, agent::WorkloadStatus & status) = 0;
+
+	/**
+	 * @brief GetChannelStatus
+	 * @param path
+	 * @param status
+	 * @return
+	 */
+	virtual ExitCode_t GetChannelStatus(
+		std::string const & path, agent::ChannelStatus & status) = 0;
+	/**
+	 * @param system_id
+	 */
+	virtual ExitCode_t GetChannelStatus(
+		int system_id, agent::ChannelStatus & status) = 0;
+
+
+	// ------------- Multi-remote management functions ------------------
+
+	/**
+	 * @brief SendJoinRequest
+	 * @param path
+	 * @return
+	 */
+	virtual ExitCode_t SendJoinRequest(std::string const & path) = 0;
+	/**
+	 * @param system_id
+	 */
+	virtual ExitCode_t SendJoinRequest(int system_id) = 0;
+
+	/**
+	 * @brief SendDisjoinRequest
+	 * @param path
+	 * @return
+	 */
+	virtual ExitCode_t SendDisjoinRequest(std::string const & path) = 0;
+	/**
+	 * @param system_id
+	 */
+	virtual ExitCode_t SendDisjoinRequest(int system_id) = 0;
+
+
+	// ----------- Scheduling / Resource allocation functions ----------
+
+	/**
+	* @brief SendScheduleRequest
+	* @param path
+	* @param request
+	* @return
+	*/
+	virtual ExitCode_t SendScheduleRequest(
+		std::string const & path,
+		agent::ApplicationScheduleRequest const & request) = 0;
+
+};
+
+} // namespace plugins
+
+} // namespace bbque
+
+#endif // BBQUE_REMOTE_PROXY_IF_H
---
+++ include/bbque/plugins/agent_proxy_types.h
@@ -0,0 +1,71 @@
+
+#ifndef BBQUE_REMOTE_PROXY_TYPES_H_
+#define BBQUE_REMOTE_PROXY_TYPES_H_
+
+#include <ctype.h>
+#include <bitset>
+#include <map>
+#include <string>
+
+#include "bbque/config.h"
+#include "bbque/res/resource_type.h"
+
+namespace bbque
+{
+namespace agent
+{
+
+/**
+ * @enum ExitCode_t
+ */
+enum class ExitCode_t {
+	OK,
+	AGENT_UNREACHABLE,
+	AGENT_DISCONNECTED,
+	REQUEST_REJECTED,
+};
+
+/**
+ * @struct ResourceStatus
+ */
+struct ResourceStatus {
+	uint64_t total;
+	uint64_t used;
+	int32_t power_mw;
+	int32_t temperature;
+	int16_t degradation;
+};
+
+/**
+ * @struct WorkloadStatus
+ */
+struct WorkloadStatus {
+	uint32_t nr_apps;
+	uint32_t nr_running;
+	uint32_t nr_ready;
+};
+
+/**
+ * @struct ChannelStatus
+ */
+struct ChannelStatus {
+	bool connected;
+	int32_t latency_ms;
+};
+
+/**
+ * @struct ApplicationScheduleRequest
+ */
+struct ApplicationScheduleRequest {
+	std::string name;
+	int16_t awm_id;
+	struct {
+		std::map<bbque::res::ResourceType, uint64_t> amount;
+		std::map<bbque::res::ResourceType, std::bitset<BBQUE_MAX_R_ID_NUM+1>> binding_set;
+	} resources;
+};
+
+} // namespace agent
+} // namespace bbque
+
+#endif // BBQUE_REMOTE_PROXY_TYPES_H_
