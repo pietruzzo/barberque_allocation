a1083ab16d3cb458192c42035ea7c690362122e7
Giuseppe Massari <joe.massanga@gmail.com>
2016-09-06 18:22:19 +0200

    Add missing copyright form
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- include/bbque/plugins/agent_proxy_if.h
+++ include/bbque/plugins/agent_proxy_if.h
@@ -1,11 +1,28 @@
+/*
+ * Copyright (C) 2016  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef BBQUE_AGENT_PROXY_IF_H
 #define BBQUE_AGENT_PROXY_IF_H
 
-#define AGENT_PROXY_NAMESPACE "bq.gx"
-#define AGENT_PROXY_CONFIG    "AgentProxy"
-
 #include "bbque/pp/platform_description.h"
 #include "bbque/plugins/agent_proxy_types.h"
+
+#define AGENT_PROXY_NAMESPACE "bq.gx"
+#define AGENT_PROXY_CONFIG    "AgentProxy"
 
 namespace bbque
 {
