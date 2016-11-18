b6600f366c075e0a1a4b9daf857f584a2cc5e44a
Giuseppe Massari <joe.massanga@gmail.com>
2016-11-18 13:48:02 +0100

    [SchedPol_Test] Testing purpose policy
    This is a policy for testing purpose. This means that the framework
    developer can directly put the hands inside to change internal function
    calls arguments in order to test the correct behaviour of the BarbequeRTRM
    internals involved.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ plugins/schedpol/test/test_schedpol.cc
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) 2016  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "test_schedpol.h"
+
+#include <cstdlib>
+#include <cstdint>
+#include <iostream>
+
+#include "bbque/modules_factory.h"
+#include "bbque/utils/logging/logger.h"
+
+#include "bbque/app/working_mode.h"
+#include "bbque/res/binder.h"
+#include "bbque/res/resource_path.h"
+
+#define MODULE_CONFIG SCHEDULER_POLICY_CONFIG "." SCHEDULER_POLICY_NAME
+
+namespace bu = bbque::utils;
+namespace po = boost::program_options;
+
+namespace bbque { namespace plugins {
+
+// :::::::::::::::::::::: Static plugin interface ::::::::::::::::::::::::::::
+
+void * TestSchedPol::Create(PF_ObjectParams *) {
+	return new TestSchedPol();
+}
+
+int32_t TestSchedPol::Destroy(void * plugin) {
+	if (!plugin)
+		return -1;
+	delete (TestSchedPol *)plugin;
+	return 0;
+}
+
+// ::::::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
+
+char const * TestSchedPol::Name() {
+	return SCHEDULER_POLICY_NAME;
+}
+
+TestSchedPol::TestSchedPol():
+		cm(ConfigurationManager::GetInstance()),
+		ra(ResourceAccounter::GetInstance()) {
+	logger = bu::Logger::GetLogger("bq.sp.test");
+	assert(logger);
+	if (logger)
+		logger->Info("test: Built a new dynamic object[%p]", this);
+	else
+		fprintf(stderr,
+			FI("test: Built new dynamic object [%p]\n"), (void *)this);
+}
+
+
+TestSchedPol::~TestSchedPol() {
+
+}
+
+
+SchedulerPolicyIF::ExitCode_t TestSchedPol::Init() {
+	// Build a string path for the resource state view
+	std::string token_path(MODULE_NAMESPACE);
+	++status_view_count;
+	token_path.append(std::to_string(status_view_count));
+	logger->Debug("Init: Require a new resource state view [%s]",
+		token_path.c_str());
+
+	// Get a fresh resource status view
+	ResourceAccounterStatusIF::ExitCode_t ra_result =
+		ra.GetView(token_path, sched_status_view);
+	if (ra_result != ResourceAccounterStatusIF::RA_SUCCESS) {
+		logger->Fatal("Init: cannot get a resource state view");
+		return SCHED_ERROR_VIEW;
+	}
+	logger->Debug("Init: resources state view token: %ld", sched_status_view);
+
+	return SCHED_OK;
+}
+
+
+SchedulerPolicyIF::ExitCode_t
+TestSchedPol::Schedule(
+		System & system,
+		RViewToken_t & status_view) {
+	SchedulerPolicyIF::ExitCode_t result = SCHED_DONE;
+
+	// Class providing query functions for applications and resources
+	sys = &system;
+	Init();
+
+	/** INSERT YOUR CODE HERE **/
+	bbque::app::AppCPtr_t papp;
+	AppsUidMapIt app_it;
+
+	// Ready applications
+	papp = sys->GetFirstReady(app_it);
+	for (; papp; papp = sys->GetNextReady(app_it)) {
+		AssignWorkingMode(papp);
+	}
+
+	// Running applications
+	papp = sys->GetFirstRunning(app_it);
+	for (; papp; papp = sys->GetNextRunning(app_it)) {
+		papp->CurrentAWM()->ClearResourceRequests();
+		AssignWorkingMode(papp);
+	}
+
+	// Return the new resource status view according to the new resource
+	// allocation performed
+	status_view = sched_status_view;
+	return result;
+}
+
+SchedulerPolicyIF::ExitCode_t
+TestSchedPol::AssignWorkingMode(bbque::app::AppCPtr_t papp) {
+
+	// Build a new working mode featuring assigned resources
+	ba::AwmPtr_t pawm = papp->CurrentAWM();
+	if (pawm == nullptr) {
+		pawm = std::make_shared<ba::WorkingMode>(
+				papp->WorkingModes().size(),"Run-time", 1, papp);
+	}
+
+	// 
+	pawm->AddResourceRequest("sys0.cpu.pe", 200, br::ResourceAssignment::Policy::BALANCED);
+
+	// The ResourceBitset object is used for the processing elements binding
+	// (CPU core mapping)
+	br::ResourceBitset pes;
+	pes.Set(0);
+
+	int32_t ref_num = -1;
+
+	// CPU-level binding: the processing elements are in the scope of CPU '1'
+	ref_num = pawm->BindResource(br::ResourceType::CPU, R_ID_ANY, 1, ref_num);
+	auto resource_path = ra.GetPath("sys0.cpu1.pe");
+
+	ref_num = pawm->BindResource(resource_path, pes, ref_num);
+	logger->Info("Reference number for binding: %d", ref_num);
+
+	pes.Set(1);
+	ref_num = pawm->BindResource(resource_path, pes, ref_num);
+	logger->Info("Reference number for binding: %d", ref_num);
+
+	papp->ScheduleRequest(pawm, sched_status_view, ref_num);
+
+	/*
+	// Enqueue scheduling entity
+	SchedEntityPtr_t psched = std::make_shared<SchedEntity_t>(
+			papp, pawm, R_ID_ANY, 0);
+	entities.push_back(psched);
+	*/
+
+	return SCHED_OK;
+}
+
+
+
+} // namespace plugins
+
+} // namespace bbque
---
+++ plugins/schedpol/test/test_schedpol.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2016  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BBQUE_TEST_SCHEDPOL_H_
+#define BBQUE_TEST_SCHEDPOL_H_
+
+#include <cstdint>
+#include <list>
+#include <memory>
+
+#include "bbque/configuration_manager.h"
+#include "bbque/plugins/plugin.h"
+#include "bbque/plugins/scheduler_policy.h"
+#include "bbque/scheduler_manager.h"
+
+#define SCHEDULER_POLICY_NAME "test"
+
+#define MODULE_NAMESPACE SCHEDULER_POLICY_NAMESPACE "." SCHEDULER_POLICY_NAME
+
+using bbque::res::RViewToken_t;
+using bbque::utils::MetricsCollector;
+using bbque::utils::Timer;
+
+// These are the parameters received by the PluginManager on create calls
+struct PF_ObjectParams;
+
+namespace bbque { namespace plugins {
+
+class LoggerIF;
+
+/**
+ * @class TestSchedPol
+ *
+ * Test scheduler policy registered as a dynamic C++ plugin.
+ */
+class TestSchedPol: public SchedulerPolicyIF {
+
+public:
+
+	// :::::::::::::::::::::: Static plugin interface :::::::::::::::::::::::::
+
+	/**
+	 * @brief Create the test plugin
+	 */
+	static void * Create(PF_ObjectParams *);
+
+	/**
+	 * @brief Destroy the test plugin 
+	 */
+	static int32_t Destroy(void *);
+
+
+	// :::::::::::::::::: Scheduler policy module interface :::::::::::::::::::
+
+	/**
+	 * @brief Destructor
+	 */
+	virtual ~TestSchedPol();
+
+	/**
+	 * @brief Return the name of the policy plugin
+	 */
+	char const * Name();
+
+
+	/**
+	 * @brief The member function called by the SchedulerManager to perform a
+	 * new scheduling / resource allocation
+	 */
+	ExitCode_t Schedule(System & system, RViewToken_t & status_view);
+
+private:
+
+	/** Configuration manager instance */
+	ConfigurationManager & cm;
+
+	/** Resource accounter instance */
+	ResourceAccounter & ra;
+
+	/** System logger instance */
+	std::unique_ptr<bu::Logger> logger;
+
+
+	/**
+	 * @brief Constructor
+	 *
+	 * Plugins objects could be build only by using the "create" method.
+	 * Usually the PluginManager acts as object
+	 */
+	TestSchedPol();
+
+	/**
+	 * @brief Optional initialization member function
+	 */
+	ExitCode_t Init();
+
+	SchedulerPolicyIF::ExitCode_t AssignWorkingMode(bbque::app::AppCPtr_t papp);
+};
+
+} // namespace plugins
+
+} // namespace bbque
+
+#endif // BBQUE_TEST_SCHEDPOL_H_
