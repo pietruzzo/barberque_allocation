90d26e2a58a66faa57ddc7090f60f9bb2f97e26c
Giuseppe Massari <joe.massanga@gmail.com>
2017-03-10 10:10:43 +0100

    [APP] Minor change
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- bbque/app/application.cc
+++ bbque/app/application.cc
@@ -90,6 +90,8 @@ Application::Application(std::string const & _name,
 	// Format the EXC string identifier
 	snprintf(str_id, APPLICATION_NAME_LEN, "%05d:%5s:%02d",
 		Pid(), Name().substr(0,5).c_str(), ExcId());
+
+	// Task-graph output serial file path
 	tg_path.assign(BBQUE_TG_FILE_PREFIX + std::string(str_id).substr(0, 6) + Name());
 	logger->Info("Task-graph serial file@: <%s>", tg_path.c_str());
 
