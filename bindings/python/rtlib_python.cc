544ce5d86eee0cb69d5d337935d6b1c65246f30a
Enrico Ghirardi <i@choco.me>
2017-03-19 18:43:45 +0000

    [Bindings/Python] Add remaining bindings for BBqueEXC
    Add remaining bindings for missing BBqueEXC class and add necessary
    wrappers to use them from Python
    
    Signed-off-by: Enrico Ghirardi <i@choco.me>

--- bindings/python/rtlib_python.cc
+++ bindings/python/rtlib_python.cc
@@ -12,6 +12,61 @@ struct RTLIB_Services_Wrapper {
    RTLIB_Services_t *services;
 };
 
+struct RTLIB_Resources_Amount_Wrapper {
+   int32_t amount;
+};
+
+class RTLIB_Resources_Systems_Wrapper {
+   public:
+      RTLIB_Resources_Systems_Wrapper(uint16_t number_of_systems) :
+         _number_of_systems(number_of_systems) {
+         _systems.reserve(number_of_systems);
+      }
+      int32_t * systems() {
+         return &_systems[0];
+      }
+      uint16_t number_of_systems() {
+         return _number_of_systems;
+      }
+   private:
+      uint16_t _number_of_systems;
+      std::vector<int32_t> _systems;
+};
+
+class RTLIB_AffinityMasks_Wrapper {
+   public:
+      RTLIB_AffinityMasks_Wrapper(int number_of_masks) :
+         _number_of_masks(number_of_masks) {
+         _masks.reserve(number_of_masks);
+      }
+      int32_t * masks() {
+         return &_masks[0];
+      }
+      int number_of_masks() {
+         return _number_of_masks;
+      }
+   private:
+      int _number_of_masks;
+      std::vector<int32_t> _masks;
+};
+
+class RTLIB_Logger_Wrapper {
+   public:
+      RTLIB_Logger_Wrapper(std::unique_ptr<bu::Logger> &logger) :
+         w_logger(logger){}
+      void Debug(std::string msg) { w_logger->Debug(msg.c_str()); };
+      void Info(std::string msg) { w_logger->Info(msg.c_str()); };
+      void Notice(std::string msg) { w_logger->Notice(msg.c_str()); };
+      void Warn(std::string msg) { w_logger->Warn(msg.c_str()); };
+      void Error(std::string msg) { w_logger->Error(msg.c_str()); };
+      void Crit(std::string msg) { w_logger->Crit(msg.c_str()); };
+      void Alert(std::string msg) { w_logger->Alert(msg.c_str()); };
+      void Fatal(std::string msg) { w_logger->Fatal(msg.c_str()); };
+
+   private:
+      std::unique_ptr<bu::Logger> &w_logger;
+};
+
 class PyBbqueEXC : public BbqueEXC {
    public:
       using BbqueEXC::BbqueEXC;
@@ -73,6 +128,16 @@ class PyBbqueEXC : public BbqueEXC {
                BbqueEXC,
                onRelease,
                );
+      };
+
+      std::unique_ptr<bu::Logger>& get_logger() {
+         return logger;
+      };
+      std::string const get_exc_name() {
+         return exc_name;
+      };
+      std::string const get_rpc_name() {
+         return rpc_name;
       };
 };
 
@@ -146,7 +211,50 @@ PYBIND11_PLUGIN(barbeque) {
       .value("ACCELERATOR", RTLIB_ResourceType::ACCELERATOR)
       .export_values();
 
-   // Our wrapped RTLIB_Init function
+   py::enum_<RTLIB_ConstraintOperation>(m, "RTLIB_ConstraintOperation")
+      .value("CONSTRAINT_REMOVE", RTLIB_ConstraintOperation::CONSTRAINT_REMOVE)
+      .value("CONSTRAINT_ADD", RTLIB_ConstraintOperation::CONSTRAINT_ADD)
+      .export_values();
+
+   py::enum_<RTLIB_ConstraintType>(m, "RTLIB_ConstraintType")
+      .value("LOWER_BOUND", RTLIB_ConstraintType::LOWER_BOUND)
+      .value("UPPER_BOUND", RTLIB_ConstraintType::UPPER_BOUND)
+      .value("EXACT_VALUE", RTLIB_ConstraintType::EXACT_VALUE)
+      .export_values();
+
+   py::class_<RTLIB_Constraint>(m, "RTLIB_Constraint")
+      .def_readwrite("awm", &RTLIB_Constraint::awm)
+      .def_readwrite("operation", &RTLIB_Constraint::operation)
+      .def_readwrite("type", &RTLIB_Constraint::type);
+
+   py::class_<RTLIB_WorkingModeParams>(m, "RTLIB_WorkingModeParams")
+      .def_readwrite("awm_id", &RTLIB_WorkingModeParams::awm_id)
+      .def_readwrite("services", &RTLIB_WorkingModeParams::services)
+      .def_readwrite("nr_sys", &RTLIB_WorkingModeParams::nr_sys)
+      .def_readwrite("systems", &RTLIB_WorkingModeParams::systems);
+
+   py::class_<RTLIB_Resources_Amount_Wrapper>(m, "RTLIB_Resources_Amount_Wrapper")
+      .def(py::init<>())
+      .def_readwrite("amount", &RTLIB_Resources_Amount_Wrapper::amount);
+
+   py::class_<RTLIB_Resources_Systems_Wrapper>(m, "RTLIB_Resources_Systems_Wrapper")
+      .def(py::init<uint16_t>())
+      .def("systems", &RTLIB_Resources_Systems_Wrapper::systems);
+
+   py::class_<RTLIB_AffinityMasks_Wrapper>(m, "RTLIB_AffinityMasks_Wrapper")
+      .def(py::init<int>())
+      .def("masks", &RTLIB_AffinityMasks_Wrapper::masks);
+
+   py::class_<RTLIB_Logger_Wrapper>(m, "RTLIB_Logger_Wrapper")
+      .def("Debug", &RTLIB_Logger_Wrapper::Debug)
+      .def("Info", &RTLIB_Logger_Wrapper::Info)
+      .def("Notice", &RTLIB_Logger_Wrapper::Notice)
+      .def("Warn", &RTLIB_Logger_Wrapper::Warn)
+      .def("Erro", &RTLIB_Logger_Wrapper::Error)
+      .def("Crit", &RTLIB_Logger_Wrapper::Crit)
+      .def("Alert", &RTLIB_Logger_Wrapper::Alert)
+      .def("Fatal", &RTLIB_Logger_Wrapper::Fatal);
+
    m.def("RTLIB_Init",
          [](const char *name, RTLIB_Services_Wrapper &services) {
             return RTLIB_Init(name, &services.services);
@@ -163,14 +271,32 @@ PYBIND11_PLUGIN(barbeque) {
       .def("Terminate", &BbqueEXC::Terminate)
       .def("isRegistered", &BbqueEXC::isRegistered)
       .def("Enable", &BbqueEXC::Enable)
-      .def("SetAWMConstraints", &BbqueEXC::SetAWMConstraints)
+      .def("SetAWMConstraints",
+            [](BbqueEXC &bbqueEXC, std::vector<RTLIB_Constraint> constraints)
+            {
+               return bbqueEXC.SetAWMConstraints(&constraints[0], constraints.size());
+            })
       .def("ClearAWMConstraints", &BbqueEXC::ClearAWMConstraints)
       .def("SetGoalGap", &BbqueEXC::SetGoalGap)
       .def("GetUniqueID_String", &BbqueEXC::GetUniqueID_String)
       .def("GetUniqueID", &BbqueEXC::GetUniqueID)
-      .def("GetAssignedResources", (RTLIB_ExitCode (BbqueEXC::*)(RTLIB_ResourceType, int32_t &))&BbqueEXC::GetAssignedResources)
-      .def("GetAssignedResources", (RTLIB_ExitCode (BbqueEXC::*)(RTLIB_ResourceType, int32_t *, uint16_t))&BbqueEXC::GetAssignedResources)
-      .def("GetAffinityMask", &BbqueEXC::GetAffinityMask)
+      .def("GetAssignedResources",
+            [](BbqueEXC &bbqueEXC, RTLIB_ResourceType r_type,
+               RTLIB_Resources_Amount_Wrapper &r_amount_w)
+            {
+               return bbqueEXC.GetAssignedResources(r_type, r_amount_w.amount);
+            })
+      .def("GetAssignedResources",
+            [](BbqueEXC &bbqueEXC, RTLIB_ResourceType r_type,
+               RTLIB_Resources_Systems_Wrapper &r_systems_w)
+            {
+               return bbqueEXC.GetAssignedResources(r_type, r_systems_w.systems(), r_systems_w.number_of_systems());
+            })
+      .def("GetAffinityMask",
+            [](BbqueEXC &bbqueEXC, RTLIB_AffinityMasks_Wrapper &a_masks_w)
+            {
+               return bbqueEXC.GetAffinityMask(a_masks_w.masks(), a_masks_w.number_of_masks());
+            })
       .def("SetCPS", &BbqueEXC::SetCPS)
       .def("SetCPSGoal", (RTLIB_ExitCode (BbqueEXC::*)(float, float))&BbqueEXC::SetCPSGoal)
       .def("SetJPSGoal", &BbqueEXC::SetJPSGoal)
@@ -184,7 +310,14 @@ PYBIND11_PLUGIN(barbeque) {
       .def("WorkingModeParams", &BbqueEXC::WorkingModeParams)
       .def("Done", &BbqueEXC::Done)
       .def("CurrentAWM", &BbqueEXC::CurrentAWM)
-      .def("Configuration", &BbqueEXC::Configuration);
+      .def("Configuration", &BbqueEXC::Configuration)
+      .def_property_readonly("exc_name", &PyBbqueEXC::get_exc_name)
+      .def_property_readonly("rpc_name", &PyBbqueEXC::get_rpc_name)
+      .def_property_readonly("logger", [](PyBbqueEXC &bbqueEXC)
+            {
+               auto logger_w = new RTLIB_Logger_Wrapper(bbqueEXC.get_logger());
+               return logger_w;
+            }, py::return_value_policy::take_ownership);
 
    m.attr("__version__") = py::str("dev");
 
