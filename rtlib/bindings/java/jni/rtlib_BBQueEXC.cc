529609107c6cbdf645f6a1c4f46277b9f49fc5b6
Unknown <motardx94@gmail.com>
2018-02-21 18:53:36 +0100

    [Bindings/Java] JNI code in progress
    This commit contains an implementation of all simple method to wrap.

--- rtlib/bindings/java/jni/rtlib_BBQueEXC.cc
+++ rtlib/bindings/java/jni/rtlib_BBQueEXC.cc
@@ -1,43 +1,226 @@
+#include <cstdarg>
 #include <bbque/bbque_exc.h>
 #include "rtlib_BBQueEXC.h"
-#include <iostream>
 
 jlong Java_bbque_rtlib_BBQueEXC_initNative(JNIEnv *env, jobject obj, jstring javaName, jstring javaRecipe) {
 	const char *nativeName = env->GetStringUTFChars(javaName, JNI_FALSE);
 	const char *nativeRecipe = env->GetStringUTFChars(javaRecipe, JNI_FALSE);
-	std::cout << "[JNI] initializing RTLIB..." << std::endl;
 	RTLIB_Services_t *rtlib;
 	RTLIB_Init(nativeName, &rtlib);
 	jlong native_pointer;
 	if (!rtlib) {
-		std::cout << "[JNI] initializing RTLIB...FAILED!!!" << std::endl;
 		native_pointer = bbque_rtlib_BBQueEXC_ERROR_LIB_INIT;
 	} else {
-		std::cout << "[JNI] initializing RTLIB...OK" << std::endl;
-		std::cout << "[JNI] initializing APP..." << std::endl;
-		JNIBbqueEXC *jni_exc = new JNIBbqueEXC(nativeName, nativeRecipe, rtlib);
+		JNIBbqueEXC *jni_exc = new JNIBbqueEXC(nativeName, nativeRecipe, rtlib, env, obj);
 		if (!jni_exc) {
-			std::cout << "[JNI] initializing APP...FAILED!!!" << std::endl;
 			native_pointer = bbque_rtlib_BBQueEXC_ERROR_EXECUTION_CONTEXT_REGISTRATION;
 		} else {
-			std::cout << "[JNI] initializing APP...OK" << std::endl;
 			native_pointer = reinterpret_cast<jlong>(jni_exc);
 		}
 	}
-	std::cout << "[JNI] releasing memory..." << std::endl;
 	env->ReleaseStringUTFChars(javaName, nativeName);
 	env->ReleaseStringUTFChars(javaRecipe, nativeRecipe);
-	std::cout << "[JNI] returning pointer to JAVA" << std::endl;
 	return native_pointer;
 }
 
+void Java_bbque_rtlib_BBQueEXC_disposeNative(JNIEnv *env, jobject obj, jlong native_pointer) {
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	delete jni_exc;
+}
+
+jboolean Java_bbque_rtlib_BBQueEXC_isRegistered(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->isRegistered();
+}
+
 void Java_bbque_rtlib_BBQueEXC_start(JNIEnv *env, jobject obj) {
-	std::cout << "[JNI] Obtaining native pointer from JAVA..." << std::endl;
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->Start();
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_waitCompletion(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->WaitCompletion();
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_terminate(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->Terminate();
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_enable(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->Enable();
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_disable(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->Disable();
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+/*
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setAWMConstraints(JNIEnv *, jobject, jobjectArray) {
+	// TODO: implement this function
+}*/
+
+void Java_bbque_rtlib_BBQueEXC_clearAWMConstraints(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->ClearAWMConstraints();
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+/*
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setGoalGap(JNIEnv *, jobject, jint) {
+	// TODO: implement this function
+}*/
+
+jstring Java_bbque_rtlib_BBQueEXC_getUniqueID_1String(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return env->NewStringUTF(jni_exc->GetUniqueID_String());
+}
+
+/*
+JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getAssignedResources__Lbbque_rtlib_enumeration_RTLibResourceType_2(JNIEnv *, jobject, jobject) {
+	// TODO: implement this function
+}*/
+
+/*
+JNIEXPORT jintArray JNICALL Java_bbque_rtlib_BBQueEXC_getAffinityMask(JNIEnv *, jobject, jintArray) {
+	// TODO: implement this function
+}*/
+
+/*
+JNIEXPORT jintArray JNICALL Java_bbque_rtlib_BBQueEXC_getAssignedResources__Lbbque_rtlib_enumeration_RTLibResourceType_2_3I(JNIEnv *, jobject, jobject, jintArray) {
+	// TODO: implement this function
+}*/
+
+void Java_bbque_rtlib_BBQueEXC_setCPS(JNIEnv *env, jobject obj, jfloat java_cps) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->SetCPS(java_cps);
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_setJPSGoal(JNIEnv *env, jobject obj, jfloat java_jps_min, jfloat java_jps_max, jint java_jpc) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->SetJPSGoal(java_jps_min, java_jps_max, java_jpc);
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_updateJPC(JNIEnv *env, jobject obj, jint java_jpc) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->UpdateJPC(java_jpc);
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_setCPSGoal__FF(JNIEnv *env, jobject obj, jfloat java_cps_min, jfloat java_cps_max) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->SetCPSGoal(java_cps_min, java_cps_max);
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_setCPSGoal__F(JNIEnv *env, jobject obj, jfloat java_cps) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->SetCPSGoal(java_cps);
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+void Java_bbque_rtlib_BBQueEXC_setMinimumCycleTimeUs(JNIEnv *env, jobject obj, jint java_min_cycle_time_us) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	RTLIB_ExitCode_t code = jni_exc->SetMinimumCycleTimeUs(java_min_cycle_time_us);
+	throwRTLibExceptionIfNecessary(env, obj, code);
+}
+
+jfloat Java_bbque_rtlib_BBQueEXC_getCPS(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->GetCPS();
+}
+
+jfloat Java_bbque_rtlib_BBQueEXC_getJPS(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->GetJPS();
+}
+
+jint Java_bbque_rtlib_BBQueEXC_getCTimeUs(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->GetCTimeUs();
+}
+
+jint Java_bbque_rtlib_BBQueEXC_getCTimeMs(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->GetCTimeMs();
+}
+
+jint Java_bbque_rtlib_BBQueEXC_cycles(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->Cycles();
+}
+
+/*
+JNIEXPORT jobject JNICALL Java_bbque_rtlib_BBQueEXC_workingModeParams(JNIEnv *env, jobject obj) {
+	// TODO: implement this function
+}*/
+
+jboolean Java_bbque_rtlib_BBQueEXC_done(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->Done();
+}
+
+jint Java_bbque_rtlib_BBQueEXC_currentAWM(JNIEnv *env, jobject obj) {
+	jlong native_pointer = getEXCNativePointer(env, obj);
+	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
+	return jni_exc->CurrentAWM();
+}
+
+/*
+JNIEXPORT jobject JNICALL Java_bbque_rtlib_BBQueEXC_configuration(JNIEnv *env, jobject obj) {
+	// TODO: implement this function
+}*/
+
+RTLIB_ExitCode_t JNIBbqueEXC::onGenericIntCallback(const char *method, const char *signature, ...) {
+	va_list arguments;
+	va_start(arguments, signature);
+	JNIEnv *env;
+	jvm->AttachCurrentThread((void **)&env, NULL);
+	jmethodID callback_method = env->GetMethodID(callback_class, method, signature);
+	jint exit_code = env->CallIntMethodV(callback_obj, callback_method, arguments);
+	va_end(arguments);
+	jvm->DetachCurrentThread();
+	return RTLIB_OK; // TODO: find a way to convert exit codes from java to cpp
+}
+
+jlong getEXCNativePointer(JNIEnv *env, jobject obj) {
 	jclass java_class = env->GetObjectClass(obj);
 	jfieldID native_pointer_id = env->GetFieldID(java_class, "mNativePointer", "J");
-	jlong native_pointer = env->GetLongField(obj, native_pointer_id);
-	JNIBbqueEXC *jni_exc = reinterpret_cast<JNIBbqueEXC *>(native_pointer);
-	std::cout << "[JNI] the execution is starting..." << std::endl;
-	jni_exc->Start();
-	// TODO: check response and throw an exception if necessary
+	return env->GetLongField(obj, native_pointer_id);
+}
+
+void throwRTLibExceptionIfNecessary(JNIEnv *env, jobject obj, RTLIB_ExitCode_t exit_code) {
+	if (exit_code != RTLIB_OK) {
+        	// TODO: throw an RTLibException with the given exit_code (cpp to java)
+	}
 }
--- rtlib/bindings/java/jni/rtlib_BBQueEXC.h
+++ rtlib/bindings/java/jni/rtlib_BBQueEXC.h
@@ -1,6 +1,5 @@
 #include <jni.h>
 #include <bbque/bbque_exc.h>
-#include <iostream>
 
 #ifndef _Included_bbque_rtlib_BBQueEXC
 #define _Included_bbque_rtlib_BBQueEXC
@@ -11,12 +10,53 @@ class JNIBbqueEXC : public BbqueEXC {
 
 public:
 
-	JNIBbqueEXC(std::string const & name, std::string const & recipe, RTLIB_Services_t *rtlib) : BbqueEXC(name, recipe, rtlib) {}
+	JNIBbqueEXC(std::string const & name, std::string const & recipe, RTLIB_Services_t *rtlib, JNIEnv *env, jobject obj)
+												: BbqueEXC(name, recipe, rtlib) {
+		env->GetJavaVM(&jvm);
+		callback_obj = env->NewGlobalRef(obj);
+		jclass obj_class = env->GetObjectClass(obj);
+		callback_class = reinterpret_cast<jclass>(env->NewGlobalRef(obj_class));
+	}
 
-	virtual ~JNIBbqueEXC() {}
+	virtual ~JNIBbqueEXC() {
+		delete jvm;
+	}
 
 private:
 
+	JavaVM *jvm;
+	jobject callback_obj;
+	jclass callback_class;
+
+	RTLIB_ExitCode_t onSetup() override {
+		return onGenericIntCallback("onSetupCallback", "()I");
+	}
+
+	RTLIB_ExitCode_t onConfigure(int8_t awm_id) override {
+		return onGenericIntCallback("onConfigureCallback", "(I)I", (jint) awm_id);
+	}
+
+	RTLIB_ExitCode_t onSuspend() override {
+		return onGenericIntCallback("onSuspendCallback", "()I");
+	}
+
+	RTLIB_ExitCode_t onResume() override {
+		return onGenericIntCallback("onResumeCallback", "()I");
+	}
+
+	RTLIB_ExitCode_t onRun() override {
+		return onGenericIntCallback("onRunCallback", "()I");
+	}
+
+	RTLIB_ExitCode_t onMonitor() override {
+		return onGenericIntCallback("onMonitorCallback", "()I");
+	}
+
+	RTLIB_ExitCode_t onRelease() override {
+		return onGenericIntCallback("onReleaseCallback", "()I");
+	}
+
+	RTLIB_ExitCode_t onGenericIntCallback(const char *method, const char *signature, ...);
 };
 
 #ifdef __cplusplus
@@ -38,16 +78,14 @@ JNIEXPORT jlong JNICALL Java_bbque_rtlib_BBQueEXC_initNative(JNIEnv *, jobject, 
  * Method:    disposeNative
  * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_disposeNative
-  (JNIEnv *, jobject, jlong);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_disposeNative(JNIEnv *, jobject, jlong);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    isRegistered
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_bbque_rtlib_BBQueEXC_isRegistered
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_bbque_rtlib_BBQueEXC_isRegistered(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
@@ -61,208 +99,193 @@ JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_start(JNIEnv *, jobject);
  * Method:    waitCompletion
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_waitCompletion
-  (JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_waitCompletion(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    terminate
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_terminate
-  (JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_terminate(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    enable
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_enable
-  (JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_enable(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    disable
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_disable
-  (JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_disable(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setAWMConstraints
  * Signature: ([Lbbque/rtlib/RTLibConstraint;)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setAWMConstraints
-  (JNIEnv *, jobject, jobjectArray);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setAWMConstraints(JNIEnv *, jobject, jobjectArray);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    clearAWMConstraints
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_clearAWMConstraints
-  (JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_clearAWMConstraints(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setGoalGap
  * Signature: (I)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setGoalGap
-  (JNIEnv *, jobject, jint);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setGoalGap(JNIEnv *, jobject, jint);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getUniqueID_String
  * Signature: ()Ljava/lang/String;
  */
-JNIEXPORT jstring JNICALL Java_bbque_rtlib_BBQueEXC_getUniqueID_1String
-  (JNIEnv *, jobject);
+JNIEXPORT jstring JNICALL Java_bbque_rtlib_BBQueEXC_getUniqueID_1String(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getAssignedResources
  * Signature: (Lbbque/rtlib/enumeration/RTLibResourceType;)I
  */
-JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getAssignedResources__Lbbque_rtlib_enumeration_RTLibResourceType_2
-  (JNIEnv *, jobject, jobject);
+JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getAssignedResources__Lbbque_rtlib_enumeration_RTLibResourceType_2(JNIEnv *, jobject, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getAffinityMask
  * Signature: ([I)[I
  */
-JNIEXPORT jintArray JNICALL Java_bbque_rtlib_BBQueEXC_getAffinityMask
-  (JNIEnv *, jobject, jintArray);
+JNIEXPORT jintArray JNICALL Java_bbque_rtlib_BBQueEXC_getAffinityMask(JNIEnv *, jobject, jintArray);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getAssignedResources
  * Signature: (Lbbque/rtlib/enumeration/RTLibResourceType;[I)[I
  */
-JNIEXPORT jintArray JNICALL Java_bbque_rtlib_BBQueEXC_getAssignedResources__Lbbque_rtlib_enumeration_RTLibResourceType_2_3I
-  (JNIEnv *, jobject, jobject, jintArray);
+JNIEXPORT jintArray JNICALL Java_bbque_rtlib_BBQueEXC_getAssignedResources__Lbbque_rtlib_enumeration_RTLibResourceType_2_3I(JNIEnv *, jobject, jobject, jintArray);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setCPS
  * Signature: (F)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setCPS
-  (JNIEnv *, jobject, jfloat);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setCPS(JNIEnv *, jobject, jfloat);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setJPSGoal
  * Signature: (FFI)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setJPSGoal
-  (JNIEnv *, jobject, jfloat, jfloat, jint);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setJPSGoal(JNIEnv *, jobject, jfloat, jfloat, jint);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    updateJPC
  * Signature: (I)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_updateJPC
-  (JNIEnv *, jobject, jint);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_updateJPC(JNIEnv *, jobject, jint);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setCPSGoal
  * Signature: (FF)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setCPSGoal__FF
-  (JNIEnv *, jobject, jfloat, jfloat);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setCPSGoal__FF(JNIEnv *, jobject, jfloat, jfloat);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setCPSGoal
  * Signature: (F)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setCPSGoal__F
-  (JNIEnv *, jobject, jfloat);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setCPSGoal__F(JNIEnv *, jobject, jfloat);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    setMinimumCycleTimeUs
  * Signature: (I)V
  */
-JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setMinimumCycleTimeUs
-  (JNIEnv *, jobject, jint);
+JNIEXPORT void JNICALL Java_bbque_rtlib_BBQueEXC_setMinimumCycleTimeUs(JNIEnv *, jobject, jint);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getCPS
  * Signature: ()F
  */
-JNIEXPORT jfloat JNICALL Java_bbque_rtlib_BBQueEXC_getCPS
-  (JNIEnv *, jobject);
+JNIEXPORT jfloat JNICALL Java_bbque_rtlib_BBQueEXC_getCPS(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getJPS
  * Signature: ()F
  */
-JNIEXPORT jfloat JNICALL Java_bbque_rtlib_BBQueEXC_getJPS
-  (JNIEnv *, jobject);
+JNIEXPORT jfloat JNICALL Java_bbque_rtlib_BBQueEXC_getJPS(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getCTimeUs
  * Signature: ()I
  */
-JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getCTimeUs
-  (JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getCTimeUs(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    getCTimeMs
  * Signature: ()I
  */
-JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getCTimeMs
-  (JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_getCTimeMs(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    cycles
  * Signature: ()I
  */
-JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_cycles
-  (JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_cycles(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    workingModeParams
  * Signature: ()Lbbque/rtlib/RTLibWorkingModeParams;
  */
-JNIEXPORT jobject JNICALL Java_bbque_rtlib_BBQueEXC_workingModeParams
-  (JNIEnv *, jobject);
+JNIEXPORT jobject JNICALL Java_bbque_rtlib_BBQueEXC_workingModeParams(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    done
  * Signature: ()Z
  */
-JNIEXPORT jboolean JNICALL Java_bbque_rtlib_BBQueEXC_done
-  (JNIEnv *, jobject);
+JNIEXPORT jboolean JNICALL Java_bbque_rtlib_BBQueEXC_done(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    currentAWM
  * Signature: ()I
  */
-JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_currentAWM
-  (JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_bbque_rtlib_BBQueEXC_currentAWM(JNIEnv *, jobject);
 
 /*
  * Class:     bbque_rtlib_BBQueEXC
  * Method:    configuration
  * Signature: ()Lbbque/rtlib/RTLibConf;
  */
-JNIEXPORT jobject JNICALL Java_bbque_rtlib_BBQueEXC_configuration
-  (JNIEnv *, jobject);
+JNIEXPORT jobject JNICALL Java_bbque_rtlib_BBQueEXC_configuration(JNIEnv *, jobject);
+
+/*
+ * This function is used to extract the native pointer from the java instance of the EXC
+ * and return it as a jlong object.
+ */
+jlong getEXCNativePointer(JNIEnv *env, jobject obj);
+
+/*
+ * This function checks if is necessary to throw a java exception looking at the provided exit code.
+ */
+void throwRTLibExceptionIfNecessary(JNIEnv *env, jobject obj, RTLIB_ExitCode_t exit_code);
 
 #ifdef __cplusplus
 }
